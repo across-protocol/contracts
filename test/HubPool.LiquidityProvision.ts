1e37ad43448383e93708b85ecde6c53b9aa44748
chrismaree <christopher.maree@gmail.com>
2022-01-24 14:03:50 -0800

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- test/HubPool.LiquidityProvision.ts
+++ test/HubPool.LiquidityProvision.ts
@@ -1,33 +1,35 @@
 import { expect } from "chai";
 import { Contract } from "ethers";
 import { waffle, ethers } from "hardhat";
-const { deployContract } = waffle;
 
-import { getContract } from "./utils";
+import { getContractFactory } from "./utils";
 
 import { TokenRolesEnum, ZERO_ADDRESS } from "@uma/common";
 
 let hubPool: Contract;
 let timer: Contract;
-let l1Token1: Contract;
-let l1Token2: Contract;
+let weth: Contract;
+let usdc: Contract;
 
 describe("HubPool LiquidityProvision", function () {
   before(async function () {
     const [owner, liquidityProvider, other] = await ethers.getSigners();
+    timer = await (await getContractFactory("Timer", owner)).deploy();
+    weth = await (await getContractFactory("WETH9", owner)).deploy();
+    usdc = await (await getContractFactory("ExpandedERC20", owner)).deploy("USD Coin", "USDC", 6);
+    await usdc.addMember(TokenRolesEnum.MINTER, owner.address);
 
-    console.log("owner", owner.address);
-    timer = await deployContract(owner, await getContract("Timer"));
-    l1Token1 = await deployContract(owner, await getContract("ExpandedERC20"), ["Wrapped Ethereum", "WETH", 18]);
-    await l1Token1.addMember(TokenRolesEnum.MINTER, owner.address);
-    l1Token2 = await deployContract(owner, await getContract("ExpandedERC20"), ["USD Coin", "USDC", 6]);
-    await l1Token2.addMember(TokenRolesEnum.MINTER, owner.address);
-    hubPool = await deployContract(owner, await getContract("HubPool"), [timer.address]);
-    console.log("hubPool", hubPool.address);
+    hubPool = await (await getContractFactory("HubPool", owner)).deploy(timer.address);
   });
 
   it("Can add L1 token to whitelisted lpTokens mapping", async function () {
-    await expect((await hubPool.callStatic.lpTokens(l1Token1.address)).lpToken).to.equal(ZERO_ADDRESS);
+    await expect((await hubPool.callStatic.lpTokens(weth.address)).lpToken).to.equal(ZERO_ADDRESS);
+    await hubPool.enableL1TokenForLiquidityProvision(weth.address, true, "Across-WETH-LP-V2", "LP");
+
+    const lpTokenStruct = await hubPool.callStatic.lpTokens(weth.address);
+    await expect(lpTokenStruct.lpToken).to.not.equal(ZERO_ADDRESS);
+    await expect(lpTokenStruct.isWeth).to.equal(true);
+    await expect(lpTokenStruct.isEnabled).to.equal(true);
   });
   it("Only owner can enable L1 Tokens for liquidity provision", async function () {
     expect(await hubPool.callStatic.timerAddress()).to.equal(timer.address);
--- test/utils.ts
+++ test/utils.ts
@@ -2,12 +2,11 @@ import { getBytecode, getAbi } from "@uma/contracts-node";
 
 import { ethers } from "hardhat";
 
-export async function getContract(name: string): Promise<{ abi: any[]; bytecode: string }> {
-    try {
-      // Try fetch from the local ethers factory from HRE. If this exists then the contract is in this package.
-      const ethersFactory = await ethers.getContractFactory(name);
-      return { bytecode: ethersFactory.bytecode, abi: ethersFactory.interface as any };
-    } catch (error) { }
-    // If it does not exist then try find the contract in the UMA core package.
-  return { bytecode: getBytecode(name as any), abi: getAbi(name as any) };
+export async function getContractFactory(name: string, signer: any) {
+  try {
+    // Try fetch from the local ethers factory from HRE. If this exists then the contract is in this package.
+    return await ethers.getContractFactory(name);
+  } catch (error) {}
+  // If it does not exist then try find the contract in the UMA core package.
+  return new ethers.ContractFactory(getAbi(name as any), getBytecode(name as any), signer);
 }
