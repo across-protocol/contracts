0ec116594725d11fc9d9bdeecd846dfae1d4a9ea
chrismaree <christopher.maree@gmail.com>
2022-01-24 09:40:41 -0800

    bump
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.11",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
