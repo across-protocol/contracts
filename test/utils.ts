1b4e93cf88b01a240975d324285fdad9199c70d8
chrismaree <christopher.maree@gmail.com>
2022-01-24 11:43:29 -0800

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- test/utils.ts
+++ test/utils.ts
@@ -3,9 +3,11 @@ import { getBytecode, getAbi } from "@uma/contracts-node";
 import { ethers } from "hardhat";
 
 export async function getContract(name: string): Promise<{ abi: any[]; bytecode: string }> {
-  try {
-    const ethersFactory = await ethers.getContractFactory(name);
-    return { bytecode: ethersFactory.bytecode, abi: ethersFactory.interface as any };
-  } catch (error) {}
+    try {
+      // Try fetch from the local ethers factory from HRE. If this exists then the contract is in this package.
+      const ethersFactory = await ethers.getContractFactory(name);
+      return { bytecode: ethersFactory.bytecode, abi: ethersFactory.interface as any };
+    } catch (error) { }
+    // If it does not exist then try find the contract in the UMA core package.
   return { bytecode: getBytecode(name as any), abi: getAbi(name as any) };
 }
