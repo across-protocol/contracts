1968d627eabed8d2a61ba9ea811ceecfc3f0e441
Matt Rice <matthewcrice32@gmail.com>
2022-02-01 21:25:35 -0500

    improve: minor refactoring to formatting
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,12 @@ module.exports = {
     node: true,
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
+  extends: ["standard", "plugin:prettier/recommended", "plugin:node/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
+    "node/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }],
   },
 };
