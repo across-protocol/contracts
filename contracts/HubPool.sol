2c9853054439c7eef468ecfdcfb9beba4f44aaae
chrismaree <christopher.maree@gmail.com>
2022-02-08 11:06:13 -0800

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- contracts/HubPool.sol
+++ contracts/HubPool.sol
@@ -193,7 +193,7 @@ contract HubPool is Testable, Lockable, MultiCaller, Ownable {
 
         // TODO: Should relay message to L2 for destinationChainId and call setEnableRoute(originToken, destinationChainId, true)
 
-        emit WhitelistRoute(originToken, destinationChainId, destinationToken);
+        emit WhitelistRoute(destinationChainId, originToken, destinationToken);
     }
 
     function enableL1TokenForLiquidityProvision(address l1Token, bool isWeth) public onlyOwner {
