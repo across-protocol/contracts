0f099c03bfca9e15cd0547abbd781520abc62daf
Nick Pai <npai.nyc@gmail.com>
2022-02-18 18:53:08 -0700

    Fix deployments

--- test/MerkleLib.Fixture.ts
+++ test/MerkleLib.Fixture.ts
@@ -2,7 +2,6 @@ import { getContractFactory } from "./utils";
 import hre from "hardhat";
 
 export const merkleLibFixture = hre.deployments.createFixture(async ({ deployments }) => {
-  await deployments.fixture();
   const [signer] = await hre.ethers.getSigners();
   const merkleLib = await (await getContractFactory("MerkleLib", signer)).deploy();
   const merkleLibTest = await (
