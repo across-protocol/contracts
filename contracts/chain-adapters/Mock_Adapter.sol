d8b489106bc8fe81046af2edfbea62f662a5a7ed
Matt Rice <matthewcrice32@gmail.com>
2022-02-25 02:12:22 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- contracts/chain-adapters/Mock_Adapter.sol
+++ contracts/chain-adapters/Mock_Adapter.sol
@@ -14,18 +14,44 @@ contract Mock_Adapter is Base_Adapter {
 
     event RelayTokensCalled(address l1Token, address l2Token, uint256 amount, address to, address caller);
 
-    function relayMessage(address target, bytes memory message) external payable override onlyHubPool {
-        emit RelayMessageCalled(target, message, msg.sender);
+    Mock_Bridge public bridge;
+
+    constructor(address _hubPool) Base_Adapter(_hubPool) {
+        bridge = new Mock_Bridge();
     }
 
-    constructor(address _hubPool) Base_Adapter(_hubPool) {}
+    function relayMessage(address target, bytes memory message) external payable override {
+        bridge.bridgeMessage(target, message);
+        emit RelayMessageCalled(target, message, msg.sender);
+    }
 
     function relayTokens(
         address l1Token,
         address l2Token,
         uint256 amount,
         address to
-    ) external payable override onlyHubPool {
+    ) external payable override {
+        IERC20(l1Token).approve(address(bridge), amount);
+        bridge.bridgeTokens(l1Token, amount);
         emit RelayTokensCalled(l1Token, l2Token, amount, to, msg.sender);
+    }
+}
+
+// This contract is intended to "act like" a simple version of an L2 bridge.
+// It's primarily meant to better reflect how a true L2 bridge interaction might work to give better gas estimates.
+contract Mock_Bridge {
+    event BridgedTokens(address token, uint256 amount);
+    event BridgedMessage(address target, bytes message);
+
+    mapping(address => uint256) deposits;
+
+    function bridgeTokens(address token, uint256 amount) public {
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        deposits[token] += amount;
+        emit BridgedTokens(token, amount);
+    }
+
+    function bridgeMessage(address target, bytes memory message) public {
+        emit BridgedMessage(target, message);
     }
 }
