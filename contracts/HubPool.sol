9fb9e8be066292ebe4ff586195bc4e88fac87e33
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2022-02-28 16:39:39 -0700

    Update contracts/HubPool.sol
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>

--- contracts/HubPool.sol
+++ contracts/HubPool.sol
@@ -478,7 +478,7 @@ contract HubPool is HubPoolInterface, Testable, Lockable, MultiCaller, Ownable {
      * @notice After proposeRootBundle is called, if the any props are wrong then this proposal can be challenged.
      * Once the challenge period passes, then the roots are no longer disputable, and only executeRootBundle can be
      * called; moreover, this method can't be called again until all leafs are executed.
-     * @param bundleEvaluationBlockNumbers should contain the latest block number for _all_ chains, even if there are no
+     * @param bundleEvaluationBlockNumbers should contain the latest block number for all chains, even if there are no
      * relays contained on some of them. But the usage of this variable should be defined in an off chain UMIP.
      * @param poolRebalanceLeafCount Number of leaves contained in pool rebalance root.
      * @param relayerRefundRoot Relayer refund root to publish to Spoke Pool where a data worker can execute leaves to
