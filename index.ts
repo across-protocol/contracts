137af3551f79f2dd9c5c6e99e9816867dea5cc81
Chris Maree <christopher.maree@gmail.com>
2022-03-07 12:49:52 -0500

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./test/fixtures/SpokePool.Fixture";
-export * from "./test/fixtures/HubPool.Fixture";
-export * from "./test/fixtures/merkleLib.Fixture";
-export * from "./test/constants";
-export * from "./test/utils";
-export * from "./typechain";
