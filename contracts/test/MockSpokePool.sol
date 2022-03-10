53fe3da15ef2e9949f5dd08c92e3f976e4003168
chrismaree <christopher.maree@gmail.com>
2022-03-10 13:31:23 -0500

    feat: added additional ChainId prop for mock
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- contracts/test/MockSpokePool.sol
+++ contracts/test/MockSpokePool.sol
@@ -9,14 +9,24 @@ import "../SpokePoolInterface.sol";
  * @notice Implements abstract contract for testing.
  */
 contract MockSpokePool is SpokePoolInterface, SpokePool {
+    uint256 chainId_;
+
     constructor(
         address _crossDomainAdmin,
         address _hubPool,
         address _wethAddress,
-        address timerAddress
-    ) SpokePool(_crossDomainAdmin, _hubPool, _wethAddress, timerAddress) {}
+        address timerAddress,
+        uint256 _chainId
+    ) SpokePool(_crossDomainAdmin, _hubPool, _wethAddress, timerAddress) {
+        chainId_ = _chainId;
+    }
 
     function _bridgeTokensToHubPool(RelayerRefundLeaf memory relayerRefundLeaf) internal override {}
 
     function _requireAdminSender() internal override {}
+
+    function chainId() public view override(SpokePool, SpokePoolInterface) returns (uint256) {
+        // If chainId_ is set then return it, else do nothing and return the parent chainId().
+        return chainId_ == 0 ? super.chainId() : chainId_;
+    }
 }
