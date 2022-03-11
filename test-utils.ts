3f25b8a3b54e1ef596e9921943111ddaf3632375
chrismaree <christopher.maree@gmail.com>
2022-03-11 11:22:26 -0500

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- test-utils.ts
+++ test-utils.ts
@@ -1,5 +1,5 @@
 // Note: this file sits on a separate export path and is intended to export test utilities and code:
-// You can import it like this: import * as testUtils from "@across-protocol/contracts-v2/test-utils".
+// You can import it like this: import * as testUtils from "@across-protocol/contracts-v2/dist/test-utils".
 // This is separated because this code assumes the caller has a hardhat config because it imports
 // hardhat. For non-test code, import the standard index file:
 // import * as contracts-v2 from "@across-protocol/contracts-v2"
