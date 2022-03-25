140196f309ac080cf0b6da1c5856cdacf80e6508
nicholaspai <npai.nyc@gmail.com>
2022-03-25 11:13:17 -0400

    feat: export merkle lib helpers

--- test-utils.ts
+++ test-utils.ts
@@ -6,5 +6,6 @@
 export * from "./test/fixtures/SpokePool.Fixture";
 export * from "./test/fixtures/HubPool.Fixture";
 export * from "./test/fixtures/MerkleLib.Fixture";
+export * from "./test/MerkleLib.utils";
 export * from "./test/constants";
 export * from "./test/utils";
