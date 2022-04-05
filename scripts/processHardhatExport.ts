30f3d8adc69baa6b2988a9b45c95c89dff97fbf8
chrismaree <christopher.maree@gmail.com>
2022-04-05 22:16:50 +0200

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- scripts/processHardhatExport.ts
+++ scripts/processHardhatExport.ts
@@ -2,6 +2,9 @@ import fs from "fs";
 import path from "path";
 
 import * as deploymentExport from "../cache/massExport.json";
+
+// Prunes the hardhat export file sent to the cache directory to only contain the deployment addresses of each contract
+// over the network of chains the Across v2 contracts are deployed on. Meant to be run as part of a publish process.
 export async function run(): Promise<void> {
   const castExport = deploymentExport as any;
   console.log("Generating exports on the following networks(if they have deployments)", Object.keys(castExport));
--- src/ContractFinder.ts
+++ src/ContractFinder.ts
@@ -1,5 +1,7 @@
 import * as deployments from "../deployments/deployments.json";
 
+// Returns the deployed address of any contract on any network. Uses the pruned network file in the deployments directory.
+// Note that we dont export the contract ABI or bytecode. Implementors are expected to use the typechain artifacts.
 export function getDeployedAddress(contractName: string, networkId: number): string {
   try {
     return (deployments as any)[networkId.toString()][contractName];
