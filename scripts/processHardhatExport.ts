7535197650105ffbd881d8cbba239ee76baf6984
chrismaree <christopher.maree@gmail.com>
2022-04-05 22:23:19 +0200

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- scripts/processHardhatExport.ts
+++ scripts/processHardhatExport.ts
@@ -1,24 +1,25 @@
 import fs from "fs";
 import path from "path";
 
-import * as deploymentExport from "../cache/massExport.json";
-
 // Prunes the hardhat export file sent to the cache directory to only contain the deployment addresses of each contract
 // over the network of chains the Across v2 contracts are deployed on. Meant to be run as part of a publish process.
 export async function run(): Promise<void> {
-  const castExport = deploymentExport as any;
-  console.log("Generating exports on the following networks(if they have deployments)", Object.keys(castExport));
-  const processedOutput: { [chainid: string]: { [contractName: string]: string } } = {};
-  Object.keys(castExport).forEach((chainId) => {
-    if (castExport[chainId][0])
-      Object.keys(castExport[chainId][0].contracts).forEach((contractName) => {
-        if (!processedOutput[chainId]) processedOutput[chainId] = {};
-        processedOutput[chainId][contractName] = castExport[chainId][0]?.contracts[contractName].address;
-      });
-  });
-  console.log("Constructed the following address export for release:\n", processedOutput);
+  try {
+    const deploymentExport = require("../cache/massExport.json");
+    const castExport = deploymentExport as any;
+    console.log("Generating exports on the following networks(if they have deployments)", Object.keys(castExport));
+    const processedOutput: { [chainid: string]: { [contractName: string]: string } } = {};
+    Object.keys(castExport).forEach((chainId) => {
+      if (castExport[chainId][0])
+        Object.keys(castExport[chainId][0].contracts).forEach((contractName) => {
+          if (!processedOutput[chainId]) processedOutput[chainId] = {};
+          processedOutput[chainId][contractName] = castExport[chainId][0]?.contracts[contractName].address;
+        });
+    });
+    console.log("Constructed the following address export for release:\n", processedOutput);
 
-  fs.writeFileSync(`${path.resolve(__dirname)}/../deployments/deployments.json`, JSON.stringify(processedOutput));
+    fs.writeFileSync(`${path.resolve(__dirname)}/../deployments/deployments.json`, JSON.stringify(processedOutput));
+  } catch (error) {}
 }
 
 if (require.main === module) {
