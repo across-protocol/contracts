5d3f61edda0d8a071b21a35bb05e4dc45a3b0526
chrismaree <christopher.maree@gmail.com>
2022-04-05 21:40:16 +0200

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

---
+++ scripts/processHardhatExport.ts
@@ -0,0 +1,24 @@
+var fs = require("fs");
+const path = require("path");
+
+import * as deploymentExport from "../cache/massExport.json";
+export async function run(): Promise<void> {
+  console.log("keys", Object.keys(deploymentExport));
+  const processedOutput: { [chainid: string]: { [contractName: string]: string } } = {};
+  Object.keys(deploymentExport).forEach((chainId) => {
+    if (deploymentExport[chainId][0])
+      Object.keys(deploymentExport[chainId][0].contracts).forEach((contractName) => {
+        if (!processedOutput[chainId]) processedOutput[chainId] = {};
+        processedOutput[chainId][contractName] = deploymentExport[chainId][0]?.contracts[contractName].address;
+      });
+  });
+  console.log("Constructed the following address export for release:\n", processedOutput);
+
+  fs.writeFileSync(`${path.resolve(__dirname)}/../deployments/deployments.json`, JSON.stringify(processedOutput));
+}
+
+if (require.main === module) {
+  run().then(() => {
+    process.exit(0);
+  });
+}
--- src/ContractFinder.ts
+++ src/ContractFinder.ts
@@ -1,8 +1,9 @@
-import * as deploymentExport from "../deployments/export.json";
+import * as typeChain from "../typechain";
+import * as deploymentExport from "../deployments/deployments.json";
 
 export function getContractArtifact(contractName: string, networkId: number) {
   try {
-    return deploymentExport[networkId.toString()][0].contracts[contractName];
+    return { artifact: [typeChain[contractName]], address: deploymentExport[networkId.toString()][contractName] };
   } catch (error) {
     throw new Error(`Contract ${contractName} not found on ${networkId} in export.json`);
   }
