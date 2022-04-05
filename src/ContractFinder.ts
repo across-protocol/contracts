b71df6a54e87cddad0c51631d08275363176c481
chrismaree <christopher.maree@gmail.com>
2022-04-05 21:46:07 +0200

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- src/ContractFinder.ts
+++ src/ContractFinder.ts
@@ -1,16 +1,9 @@
 import * as typeChain from "../typechain";
 import * as deploymentExport from "../deployments/deployments.json";
 
-function getDeploymentAddress(contractName: string, networkId: number) {
-  try {
-    return deploymentExport[networkId.toString()][contractName];
-  } catch (error) {
-    throw new Error(`Contract ${contractName} not found on ${networkId} in export.json`);
-  }
-}
 export function getContractArtifact(contractName: string, networkId: number) {
   try {
-    return { artifact: [typeChain[contractName]], address: getDeploymentAddress(contractName, networkId) };
+    return { artifact: [typeChain[contractName]], address: deploymentExport[networkId.toString()][contractName] };
   } catch (error) {
     throw new Error(`Contract ${contractName} not found on ${networkId} in export.json`);
   }
