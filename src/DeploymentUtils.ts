219fefe7f62df09abd6acfdca49076f7dc6a87ae
chrismaree <christopher.maree@gmail.com>
2022-05-10 11:28:35 +0200

    feat: add reverse look up for contract info from address
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- src/DeploymentUtils.ts
+++ src/DeploymentUtils.ts
@@ -17,3 +17,24 @@ export function getDeployedBlockNumber(contractName: string, networkId: number):
     throw new Error(`Contract ${contractName} not found on ${networkId} in deployments.json`);
   }
 }
+
+export function getContractInfoFromAddress(searchedForAddress: string): { chainId: Number; contractName: string } {
+  try {
+    let chainId = 0;
+    let contractName = "";
+    const allChainDeployments = deployments as any;
+
+    Object.keys(allChainDeployments).forEach((_chainId) =>
+      Object.keys(allChainDeployments[_chainId]).forEach((_contractName) => {
+        if (allChainDeployments[_chainId][_contractName].address == searchedForAddress) {
+          chainId = Number(_chainId);
+          contractName = _contractName;
+          return;
+        }
+      })
+    );
+    return { chainId, contractName };
+  } catch (_) {
+    throw new Error(`Contract ${searchedForAddress} was not found in deployments.`);
+  }
+}
