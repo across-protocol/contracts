3f77820bd0e5ba485ab14e3ef54c206dd9ff4dc6
chrismaree <christopher.maree@gmail.com>
2022-05-18 14:07:26 +0200

    wip
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- contracts/Boba_SpokePool.sol
+++ contracts/Boba_SpokePool.sol
@@ -1,26 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "./Optimism_SpokePool.sol";
+import "./Ovm_SpokePool.sol";
 
 /**
- * @notice Boba Spoke pool. Exact copy of the Optimism_SpokePool with no modifications except for them swapping around
- * the definition of the l2Eth and l2Weth vs Optimism.
+ * @notice Boba Spoke pool. Note that the l2ETH and l2WETH are the opposite as that in Optimism.
  */
-contract Boba_SpokePool is Optimism_SpokePool {
+contract Boba_SpokePool is Ovm_SpokePool {
     /**
      * @notice Construct the OVM Boba SpokePool.
      * @param _crossDomainAdmin Cross domain admin to set. Can be changed by admin.
      * @param _hubPool Hub pool address to set. Can be changed by admin.
      * @param timerAddress Timer address to set.
      */
-
-    address public immutable override l2Eth = address(0x4200000000000000000000000000000000000006);
-    address public immutable override wrappedNativeToken = (0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000);
-
     constructor(
         address _crossDomainAdmin,
         address _hubPool,
         address timerAddress
-    ) Optimism_SpokePool(_crossDomainAdmin, _hubPool, timerAddress) {}
+    )
+        Ovm_SpokePool(
+            _crossDomainAdmin,
+            _hubPool,
+            timerAddress,
+            0x4200000000000000000000000000000000000006,
+            0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000
+        )
+    {}
 }
