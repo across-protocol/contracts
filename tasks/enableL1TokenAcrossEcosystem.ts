934ed80897712b141d2703bffcb636575cdab28d
Matt Rice <matthewcrice32@gmail.com>
2022-05-22 20:48:22 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- tasks/enableL1TokenAcrossEcosystem.ts
+++ tasks/enableL1TokenAcrossEcosystem.ts
@@ -1,4 +1,4 @@
-import { task, types } from "hardhat/config";
+import { task } from "hardhat/config";
 import assert from "assert";
 import { findL2TokenForL1Token, askYesNoQuestion, zeroAddress, minimalSpokePoolInterface } from "./utils";
 
