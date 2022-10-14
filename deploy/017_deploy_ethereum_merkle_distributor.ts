a81ebc763a7885d529cc4a32d6e094fef6f4f532
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-10-14 15:57:28 +0200

    feat: goerli deployment MerkleDistributor (#189)
    * feat: goerli deployment MerkleDistributor
    
    * revert applied auto changes
    
    * Revert "revert applied auto changes"
    
    This reverts commit 24bff4e418f5fef4dafc6302b89c2c87e936523f.

---
+++ deploy/017_deploy_ethereum_merkle_distributor.ts
@@ -0,0 +1,19 @@
+import "hardhat-deploy";
+import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
+
+const func = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("MerkleDistributor", {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+    args: [],
+  });
+};
+
+module.exports = func;
+func.tags = ["MerkleDistributor", "mainnet"];
