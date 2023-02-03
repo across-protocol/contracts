74a239f0eb4fd88e382fcfd2d394748272c43061
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-02-03 16:49:31 +0100

    feat: eip-712 support (#222)
    * feat: eip-712 support
    
    Signed-off-by: Dong-Ha Kim <dongha.kim210@gmail.com>
    
    * refactor: pass domain name and version to abstract SpokePool
    
    Signed-off-by: Dong-Ha Kim <dongha.kim210@gmail.com>
    
    * fixup
    
    Signed-off-by: Dong-Ha Kim <dongha.kim210@gmail.com>
    
    * refactor: remove optional `verifyingContract` type
    
    Signed-off-by: Dong-Ha Kim <dongha.kim210@gmail.com>
    
    * refactor: combine domain separator build methods
    
    Signed-off-by: Dong-Ha Kim <dongha.kim210@gmail.com>
    
    * docs: add natspec params and return to EIP712CrossChain
    
    ---------
    
    Signed-off-by: Dong-Ha Kim <dongha.kim210@gmail.com>

---
+++ contracts/EIP712CrossChain.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+/**
+ * @dev https://eips.ethereum.org/EIPS/eip-712[EIP 712] is a standard for hashing and signing of typed structured data.
+ *
+ * This contract is based on OpenZeppelin's implementation:
+ * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/utils/cryptography/EIP712.sol
+ *
+ * NOTE: Modified version that allows to build a domain separator that relies on a different chain id than the chain this
+ * contract is deployed to. An example use case we want to support is:
+ * - User A signs a message on chain with id = 1
+ * - User B executes a method by verifying user A's EIP-712 compliant signature on a chain with id != 1
+ */
+abstract contract EIP712CrossChain {
+    /* solhint-disable var-name-mixedcase */
+    bytes32 private immutable _HASHED_NAME;
+    bytes32 private immutable _HASHED_VERSION;
+    bytes32 private immutable _TYPE_HASH;
+
+    /* solhint-enable var-name-mixedcase */
+
+    /**
+     * @dev Initializes the domain separator and parameter caches.
+     *
+     * The meaning of `name` and `version` is specified in
+     * https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator[EIP 712]:
+     *
+     * - `name`: the user readable name of the signing domain, i.e. the name of the DApp or the protocol.
+     * - `version`: the current major version of the signing domain.
+     *
+     * NOTE: These parameters cannot be changed except through a xref:learn::upgrading-smart-contracts.adoc[smart
+     * contract upgrade].
+     */
+    constructor(string memory name, string memory version) {
+        bytes32 hashedName = keccak256(bytes(name));
+        bytes32 hashedVersion = keccak256(bytes(version));
+        bytes32 typeHash = keccak256("EIP712Domain(string name,string version,uint256 chainId)");
+        _HASHED_NAME = hashedName;
+        _HASHED_VERSION = hashedVersion;
+        _TYPE_HASH = typeHash;
+    }
+
+    /**
+     * @dev Returns the domain separator depending on the `originChainId`.
+     * @param originChainId Chain id of network where message originates from.
+     * @return bytes32 EIP-712-compliant domain separator.
+     */
+    function _domainSeparatorV4(uint256 originChainId) internal view returns (bytes32) {
+        return keccak256(abi.encode(_TYPE_HASH, _HASHED_NAME, _HASHED_VERSION, originChainId));
+    }
+
+    /**
+     * @dev Given an already https://eips.ethereum.org/EIPS/eip-712#definition-of-hashstruct[hashed struct], this
+     * function returns the hash of the fully encoded EIP712 message for this domain.
+     *
+     * This hash can be used together with {ECDSA-recover} to obtain the signer of a message. For example:
+     *
+     * ```solidity
+     * bytes32 structHash = keccak256(abi.encode(
+     *     keccak256("Mail(address to,string contents)"),
+     *     mailTo,
+     *     keccak256(bytes(mailContents))
+     * ));
+     * bytes32 digest = _hashTypedDataV4(structHash, originChainId);
+     * address signer = ECDSA.recover(digest, signature);
+     * ```
+     * @param structHash Hashed struct as defined in https://eips.ethereum.org/EIPS/eip-712#definition-of-hashstruct.
+     * @param originChainId Chain id of network where message originates from.
+     * @return bytes32 Hash digest that is recoverable via `EDCSA.recover`.
+     */
+    function _hashTypedDataV4(bytes32 structHash, uint256 originChainId) internal view virtual returns (bytes32) {
+        return ECDSA.toTypedDataHash(_domainSeparatorV4(originChainId), structHash);
+    }
+}
