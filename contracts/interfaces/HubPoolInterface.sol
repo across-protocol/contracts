4a947136eabf8d01f0c650384778ec102e9b19aa
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2023-03-20 15:40:49 -0400

    improve: [N06] Unused imports (#260)
    * improve: [N01] Move all interfaces under `interfaces` directories
    
    ## From Audit
    
    The Across Protocol V2 codebase contains the interfaces directory, but the
    HubPoolInterface.sol and SpokePoolInterface.sol files do not reside in that
    location. Instead, they are located in the parent directory.
    Consider moving all non-external interface files to the interfaces directory, so that
    interfaces can be more easily located.
    
    ## Developer's response
    
    I appreciate this push as I've always found it inconsistent to have `interface` files not in `interface/` folders. I've added `interface` folders to each main contract directory
    
    * improve: [N06] Unused imports
    
    ## From Audit
    
    The following imports are unused:
    Import AdapterInterface.sol in HubPoolInterface.sol
    Import ECDSA.sol in SpokePool.sol
    To improve readability and avoid confusion, consider removing any unused imports.
    
    * Update HubPoolInterface.sol

--- contracts/interfaces/HubPoolInterface.sol
+++ contracts/interfaces/HubPoolInterface.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "../chain-adapters/interfaces/AdapterInterface.sol";
-
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
