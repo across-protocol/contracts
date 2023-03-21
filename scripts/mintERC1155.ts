ee2d5c9df0594081378f98dc9a72c20a09ffdae2
Dong-Ha Kim <dongha.kim210@gmail.com>
2023-03-21 14:37:40 +0100

    feat: allow mainnet ens domains in recipients list

--- scripts/mintERC1155.ts
+++ scripts/mintERC1155.ts
@@ -1,6 +1,7 @@
 import { getContractFactory, ethers, hre } from "../test/utils";
 import { readFileSync } from "fs";
 import path from "path";
+import { getNodeUrl } from "@uma/common";
 
 const RECIPIENTS_CHUNK_SIZE = 100; // TODO: Still need to figure out which size is optimal
 
@@ -14,7 +15,7 @@ const RECIPIENTS_CHUNK_SIZE = 100; // TODO: Still need to figure out which size 
  */
 async function main() {
   const tokenId = parseInt(process.env.TOKEN_ID || "0");
-  const validRecipients = parseAndValidateRecipients();
+  const validRecipients = await parseAndValidateRecipients();
 
   const [signer] = await ethers.getSigners();
 
@@ -31,27 +32,46 @@ async function main() {
     );
     console.log("Tx hash:", airdropTx.hash);
     await airdropTx.wait();
-    console.log(`Successfully minted token to:`, recipientsChunk);
+    console.log(`Successfully minted token to chunk:`, {
+      first: recipientsChunk[0],
+      last: recipientsChunk[recipientsChunk.length - 1],
+    });
   }
 }
 
-function parseAndValidateRecipients() {
+async function parseAndValidateRecipients() {
+  const provider = new ethers.providers.JsonRpcProvider(getNodeUrl("mainnet", true, 1));
+
   if (!process.env.RECIPIENTS) {
     throw new Error("Missing path to a JSON file with the list of recipients. Pass it via env var RECIPIENTS=<PATH>");
   }
   const recipientsFilePath = path.join(__dirname, "..", process.env.RECIPIENTS);
   const recipientsFromFile: string[] = JSON.parse(readFileSync(recipientsFilePath, "utf8"));
-  const invalidRecipients = recipientsFromFile.filter((r) => !ethers.utils.isAddress(r));
+
+  const resolvedRecipients = await Promise.all(
+    recipientsFromFile.map(async (r) => {
+      if (r.toLocaleLowerCase().endsWith(".eth")) {
+        const resolvedAddress = await provider.resolveName(r);
+        if (!resolvedAddress) {
+          throw new Error(`Could not resolve ENS name: ${r}`);
+        }
+        return resolvedAddress;
+      }
+      return Promise.resolve(r);
+    })
+  );
+
+  const invalidRecipients = resolvedRecipients.filter((r) => !ethers.utils.isAddress(r));
 
   if (invalidRecipients.length > 0) {
     throw new Error(`Invalid recipients: ${invalidRecipients}`);
   }
 
-  if (new Set(recipientsFromFile).size !== recipientsFromFile.length) {
+  if (new Set(resolvedRecipients).size !== resolvedRecipients.length) {
     throw new Error("Recipients list contains duplicates");
   }
 
-  return recipientsFromFile.map((r) => ethers.utils.getAddress(r));
+  return resolvedRecipients.map((r) => ethers.utils.getAddress(r));
 }
 
 main().then(
