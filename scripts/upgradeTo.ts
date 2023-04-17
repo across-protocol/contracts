2be96ed947a1d0aea828ae0b59207af7bf310439
nicholaspai <npai.nyc@gmail.com>
2023-04-17 10:40:25 -0400

    Update upgradeTo.ts

--- scripts/upgradeTo.ts
+++ scripts/upgradeTo.ts
@@ -3,14 +3,12 @@ import { ethers, getContractFactory } from "../utils/utils";
 async function main() {
   const [signer] = await ethers.getSigners();
 
-  const spokePool = await getContractFactory("Ethereum_SpokePool", { signer });
-  const upgradeTo = spokePool.interface.encodeFunctionData("upgradeTo", ["0x74f8b450606f025A4A40507158975b22f72DE96a"]);
+  const spokePool = await getContractFactory("Arbitrum_SpokePool", { signer });
+  const upgradeTo = spokePool.interface.encodeFunctionData("upgradeTo", ["0x3CA11702f7c0F28e0b4e03C31F7492969862C569"]);
   console.log(`upgradeTo bytes: `, upgradeTo);
 
-  /// 7777 is special chain we use to test upgrades on goerli. This way we don't have to upgrade
-  // the main spoke pool we use for deposits.
   console.log(
-    `Call relaySpokePoolAdminFunction() with the params [7777, ${upgradeTo}] on this hub pool: https://goerli.etherscan.io/address/0x0e2817C49698cc0874204AeDf7c72Be2Bb7fCD5d#writeContract`
+    `Call relaySpokePoolAdminFunction() with the params [<chainId>, ${upgradeTo}] on this hub pool: https://goerli.etherscan.io/address/0x0e2817C49698cc0874204AeDf7c72Be2Bb7fCD5d#writeContract`
   );
 }
 
