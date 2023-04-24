acdb688a86a511b32ab2814db6a72e13ca21fbb0
nicholaspai <npai.nyc@gmail.com>
2023-04-24 10:50:09 -0400

    Revert "Update SpokePool.SlowRelay.ts"
    This reverts commit 59a9225715246bd4fac5a17df5a52fa8ef5198d4.

--- test/SpokePool.SlowRelay.ts
+++ test/SpokePool.SlowRelay.ts
@@ -80,22 +80,36 @@ describe("SpokePool Slow Relay Logic", async function () {
 
     // ERC20
     slowFills.push({
-      relayData: erc20LeafRelayData,
-      payoutAdjustmentPct: ethers.utils.parseEther("9"), // 10x payout.
-    });
-    slowFills.push({
-      relayData: { ...erc20LeafRelayData, message: "0x" },
-      payoutAdjustmentPct: ethers.utils.parseEther("9"), // 10x payout.
+      relayData: {
+        depositor: depositor.address,
+        recipient: recipient.address,
+        destinationToken: destErc20.address,
+        amount: consts.amountToRelay,
+        originChainId: consts.originChainId.toString(),
+        destinationChainId: consts.destinationChainId.toString(),
+        realizedLpFeePct: consts.realizedLpFeePct,
+        relayerFeePct: consts.depositRelayerFeePct,
+        depositId: consts.firstDepositId.toString(),
+        message: erc20Message,
+      },
+      payoutAdjustmentPct: ethers.utils.parseEther("9").toString(), // 10x payout.
     });
 
     // WETH
     slowFills.push({
-      relayData: wethLeafRelayData,
-      payoutAdjustmentPct: ethers.utils.parseEther("-0.5"), // 50% payout.
-    });
-    slowFills.push({
-      relayData: { ...wethLeafRelayData, message: "0x" },
-      payoutAdjustmentPct: ethers.utils.parseEther("-0.5"), // 50% payout.
+      relayData: {
+        depositor: depositor.address,
+        recipient: recipient.address,
+        destinationToken: weth.address,
+        amount: consts.amountToRelay,
+        originChainId: consts.originChainId.toString(),
+        destinationChainId: consts.destinationChainId.toString(),
+        realizedLpFeePct: consts.realizedLpFeePct,
+        relayerFeePct: consts.depositRelayerFeePct,
+        depositId: consts.firstDepositId.toString(),
+        message: wethMessage,
+      },
+      payoutAdjustmentPct: ethers.utils.parseEther("-0.5").toString(), // 50% payout.
     });
 
     // Broken payout adjustment, too small.
