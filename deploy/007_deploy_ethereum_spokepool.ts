48d70189aa53be41abebcc95ee670d514b911f25
Paul <108695806+pxrl@users.noreply.github.com>
2023-04-27 14:49:30 +0200

    refactor: Consolidate new SpokePool/proxy deployment (#277)
    * feat: Support deployment and verification of upgradeable SpokePools
    
    `hardhat-upgrades` has some useful libraries we can use to deploy and verify proxies. I've tested these all out on prod.
    
    * Update deployment files
    
    * Update SpokePool.Fixture.ts
    
    * Update deployments.json
    
    * fix
    
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>
    
    * fix tests
    
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>
    
    * Update package.json
    
    * WIP
    
    * add arbitrum goerli redeployment
    
    * Update package.json
    
    * build: workaround @uma/common package browser compatibility (#273)
    
    * Move utils to src/utils
    
    * Update publish.yml
    
    * Update package.json
    
    * Update test/SpokePool.SlowRelay.ts
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Update upgradeTo.ts
    
    * WIP
    
    * Deploy mainnet
    
    * bump OZ-contracts-upgradeable version
    
    * Update package.json
    
    * Update package.json
    
    * fix: Execute fill with updated message and recipient
    
    When sped up, updated message and recipient are not used
    
    * Update SpokePool.Relay.ts
    
    * WIP
    
    * Update test/SpokePool.SlowRelay.ts
    
    Co-authored-by: Paul <108695806+pxrl@users.noreply.github.com>
    
    * Update yarn.lock
    
    * Add signer to getContractFactory call
    
    * Update Arbitrum_Adapter.json
    
    * Delete d89e3488271f97cf1ea0bdb289d28ad2.json
    
    * refactor: Consolidate new SpokePool/proxy deployment
    
    * Post-merge cleanup
    
    * lint
    
    ---------
    
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>
    Co-authored-by: nicholaspai <npai.nyc@gmail.com>
    Co-authored-by: Dong-Ha Kim <dongha.kim210@gmail.com>
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>

--- deploy/007_deploy_ethereum_spokepool.ts
+++ deploy/007_deploy_ethereum_spokepool.ts
@@ -1,43 +1,19 @@
 import "hardhat-deploy";
-
 import hre from "hardhat";
-import { getContractFactory } from "../utils";
-
+import { deployNewProxy } from "../utils";
 import { L1_ADDRESS_MAP } from "./consts";
 
 export async function printProxyVerificationInstructions() {}
 
 const func = async function () {
-  const { deployments, getChainId, upgrades, run, getNamedAccounts } = hre;
-
-  const chainId = parseInt(await getChainId());
-  const { deployer } = await getNamedAccounts();
-
-  const hubPool = await deployments.get("HubPool");
-  console.log(`Using l1 hub pool @ ${hubPool.address}`);
+  const hubPool = await hre.companionNetworks.l1.deployments.get("HubPool");
+  const chainId = await hre.getChainId();
+  console.log(`Using L1 (chainId ${chainId}) hub pool @ ${hubPool.address}`);
 
   // Initialize deposit counter to very high number of deposits to avoid duplicate deposit ID's
   // with deprecated spoke pool.
   const constructorArgs = [1_000_000, hubPool.address, L1_ADDRESS_MAP[chainId].weth];
-  const spokePool = await upgrades.deployProxy(
-    await getContractFactory("Ethereum_SpokePool", deployer),
-    constructorArgs,
-    {
-      kind: "uups",
-    }
-  );
-  const instance = await spokePool.deployed();
-  console.log(`SpokePool deployed @ ${instance.address}`);
-  const implementationAddress = await upgrades.erc1967.getImplementationAddress(instance.address);
-  console.log(`Implementation deployed @ ${implementationAddress}`);
-
-  // hardhat-upgrades overrides the `verify` task that ships with `hardhat` so that if the address passed
-  // is a proxy, hardhat will first verify the implementation and then the proxy and also link the proxy
-  // to the implementation's ABI on etherscan.
-  // https://docs.openzeppelin.com/upgrades-plugins/1.x/api-hardhat-upgrades#verify
-  await run("verify:verify", {
-    address: instance.address,
-  });
+  await deployNewProxy("Ethereum_SpokePool", constructorArgs);
 
   // Transfer ownership to hub pool.
 };
--- deploy/013_deploy_boba_spokepool.ts
+++ deploy/013_deploy_boba_spokepool.ts
@@ -1,35 +1,17 @@
 import "hardhat-deploy";
 import hre from "hardhat";
-import { getContractFactory } from "../utils";
+import { deployNewProxy } from "../utils";
 
 const func = async function () {
-  const { upgrades, companionNetworks, run, getNamedAccounts } = hre;
-
-  // Grab L1 addresses:
-  const { deployments: l1Deployments } = companionNetworks.l1;
-  const hubPool = await l1Deployments.get("HubPool");
-  console.log(`Using l1 hub pool @ ${hubPool.address}`);
+  const hubPool = await hre.companionNetworks.l1.deployments.get("HubPool");
+  const chainId = await hre.getChainId();
+  console.log(`Using L1 (chainId ${chainId}) hub pool @ ${hubPool.address}`);
 
   // Initialize deposit counter to very high number of deposits to avoid duplicate deposit ID's
   // with deprecated spoke pool.
   // Set hub pool as cross domain admin since it delegatecalls the Adapter logic.
-  const { deployer } = await getNamedAccounts();
   const constructorArgs = [1_000_000, hubPool.address, hubPool.address];
-  const spokePool = await upgrades.deployProxy(await getContractFactory("Boba_SpokePool", deployer), constructorArgs, {
-    kind: "uups",
-  });
-  const instance = await spokePool.deployed();
-  console.log(`SpokePool deployed @ ${instance.address}`);
-  const implementationAddress = await upgrades.erc1967.getImplementationAddress(instance.address);
-  console.log(`Implementation deployed @ ${implementationAddress}`);
-
-  // hardhat-upgrades overrides the `verify` task that ships with `hardhat` so that if the address passed
-  // is a proxy, hardhat will first verify the implementation and then the proxy and also link the proxy
-  // to the implementation's ABI on etherscan.
-  // https://docs.openzeppelin.com/upgrades-plugins/1.x/api-hardhat-upgrades#verify
-  await run("verify:verify", {
-    address: instance.address,
-  });
+  await deployNewProxy("Boba_SpokePool", constructorArgs);
 };
 module.exports = func;
 func.tags = ["BobaSpokePool", "boba"];
