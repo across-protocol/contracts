dbea0b74d5491615d056964d96a8058064a854b7
Matt Rice <matthewcrice32@gmail.com>
2023-04-28 18:48:50 -0400

    feat: add contract verifier
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

---
+++ contracts/ContractVerifier.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "./interfaces/SpokePoolInterface.sol";
+
+/**
+ * @notice SpokePoolVerifier is a contract that verifies that the SpokePool exists on this chain before sending ETH to it.
+ */
+contract SpokePoolVerifier {
+    using Address for address;
+
+    function deposit(
+        SpokePoolInterface spokePool,
+        address recipient,
+        address originToken,
+        uint256 amount,
+        uint256 destinationChainId,
+        int64 relayerFeePct,
+        uint32 quoteTimestamp,
+        bytes memory message,
+        uint256 maxCount
+    ) external payable {
+        require(msg.value == amount, "msg.value != amount");
+        require(address(spokePool).isContract(), "spokePool is not a contract");
+        spokePool.deposit{ value: msg.value }(
+            recipient,
+            originToken,
+            amount,
+            destinationChainId,
+            relayerFeePct,
+            quoteTimestamp,
+            message,
+            maxCount
+        );
+    }
+}
