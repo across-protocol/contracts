67a343a12abe0dc05358b1d6d87d1fea10c328bf
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-05-15 13:38:47 -0400

    improve: bump eth waffle
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

--- utils/utils.ts
+++ utils/utils.ts
@@ -183,7 +183,7 @@ export async function getParamType(contractName: string, functionName: string, p
 
 export async function createFake(contractName: string, targetAddress: string = "") {
   const contractFactory = await getContractFactory(contractName, new ethers.VoidSigner(ethers.constants.AddressZero));
-  return targetAddress !== "" ? smock.fake(contractFactory, { address: targetAddress }) : smock.fake(contractFactory);
+  return smock.fake(contractFactory, targetAddress !== "" ? { address: targetAddress } : undefined);
 }
 
 function avmL1ToL2Alias(l1Address: string) {
