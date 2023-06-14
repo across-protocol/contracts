6a7b7030f54762daf9edfdeba6f4b50108f486d4
nicholaspai <npai.nyc@gmail.com>
2023-06-14 10:47:11 -0400

    add permission splitter examples
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- contracts/PermissionSplitter.sol
+++ contracts/PermissionSplitter.sol
@@ -12,37 +12,61 @@ contract PermissionSplitter is AccessControl, MultiCaller {
     }
 
     struct PermissionsTier {
-        bytes32 role;
         uint32 liveness; // The minimum time between a proposal being made and executed.
-        uint8 minApprovals; // i.e. quorum amongst role members required to execute anything.
         uint8 tier;
     }
 
     struct Proposal {
         address target;
         bytes32 functionSig;
-        bytes params;
+        bytes functionCallData;
         ProposalStatus status;
+        uint256 proposalTime;
     }
 
     // UUID for proposals.
-    uint256 nonce;
+    uint256 public nonce;
 
     // Tracks proposed proposals that passed quorum and can be executed after liveness.
     mapping(uint256 => Proposal) public proposals;
 
-    // Maps function signatures to permission tiers, making it convenient to change params en masse to different
+    // Maps function signatures to permission tiers, making it convenient to batch change params for multiple
     // functions.
     mapping(bytes32 => uint8) public permissionTierForFunctionSig;
 
     // Maps permission tiers to their configuration.
     mapping(uint8 => PermissionsTier) public permissionsTiers;
 
-    // General dictionary where admin can associate variables with specific L1 tokens, like the Rate Model and Token
-    // Transfer Thresholds.
-    mapping(address => string) public l1TokenConfig;
+    function setPermissionTier(uint8 tier, uint32 liveness) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        permissionsTiers[tier] = PermissionsTier({ liveness: liveness, tier: tier });
+    }
 
-    // General dictionary where admin can store global variables like `MAX_POOL_REBALANCE_LEAF_SIZE` and
-    // `MAX_RELAYER_REPAYMENT_LEAF_SIZE` that off-chain agents can query.
-    mapping(bytes32 => string) public globalConfig;
+    // Only callable by someone who has correct role for the function sig they want to call.
+    function proposeAction(
+        bytes32 functionSig,
+        bytes memory functionCallData,
+        address target
+    ) external onlyRole(functionSig) {
+        proposals[nonce] = Proposal({
+            target: target,
+            functionSig: functionSig,
+            functionCallData: functionCallData,
+            status: ProposalStatus.Pending,
+            proposalTime: block.timestamp
+        });
+        nonce++;
+    }
+
+    // Can execute pending proposal after liveness period has elapsed.
+    function executeAction(uint256 proposalId) external {
+        // Need to handle fact that livenesses can change after a proposal is pending...
+        uint256 livenessForProposal = permissionsTiers[permissionTierForFunctionSig[proposals[proposalId].functionSig]]
+            .liveness;
+        require(
+            block.timestamp >= proposals[proposalId].proposalTime + livenessForProposal,
+            "Cannot execute proposal until liveness period has elapsed."
+        );
+        proposals[nonce].status = ProposalStatus.Executed;
+        // Execute the action...
+    }
 }
