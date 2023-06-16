f308f6f2d42e7e810b3222a02ef810c7ff314aa4
nicholaspai <npai.nyc@gmail.com>
2023-06-16 15:14:19 -0400

    Update PermissionSplitter.sol

--- contracts/PermissionSplitter.sol
+++ contracts/PermissionSplitter.sol
@@ -4,71 +4,31 @@ pragma solidity ^0.8.0;
 import "@uma/core/contracts/common/implementation/MultiCaller.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+// TODO: Make upgradeable.
 contract PermissionSplitter is AccessControl, MultiCaller {
-    enum ProposalStatus {
-        None,
-        Pending,
-        Executed
-    }
+    // Inherited admin role from AccessControl. Should be assigned to Across DAO multisig
+    // bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
 
-    struct PermissionsTier {
-        uint32 liveness; // The minimum time between a proposal being made and executed.
-        uint8 tier;
-    }
-
-    struct Proposal {
-        address target;
-        bytes32 functionSig;
-        bytes functionCallData;
-        ProposalStatus status;
-        uint256 proposalTime;
-    }
-
-    // UUID for proposals.
-    uint256 public nonce;
-
-    // Tracks proposed proposals that passed quorum and can be executed after liveness.
-    mapping(uint256 => Proposal) public proposals;
-
-    // Maps function signatures to permission tiers, making it convenient to batch change params for multiple
-    // functions.
+    // Maps function signatures to role identifiers, which gatekeeps access to these functions to
+    // only role holders.
     // Assumptions: Each function signature is unique, therefore the function signature itself can be used
-    // as the AccessControl.Role identifier.
-    mapping(bytes32 => uint8) public permissionTierForFunctionSig;
+    // as the AccessControl.Role identifier. This seems like a weak assumption?
+    mapping(bytes4 => bytes32) public roleForFunctionSig;
 
-    // Maps permission tiers to their configuration.
-    mapping(uint8 => PermissionsTier) public permissionsTiers;
-
-    function setPermissionTier(uint8 tier, uint32 liveness) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        permissionsTiers[tier] = PermissionsTier({ liveness: liveness, tier: tier });
-    }
-
-    // Only callable by someone who has correct role for the function sig they want to call.
-    function proposeAction(
-        bytes32 functionSig,
-        bytes memory functionCallData,
-        address target
-    ) external onlyRole(functionSig) {
-        proposals[nonce] = Proposal({
-            target: target,
-            functionSig: functionSig,
-            functionCallData: functionCallData,
-            status: ProposalStatus.Pending,
-            proposalTime: block.timestamp
-        });
-        nonce++;
-    }
-
+    // TODO: Implement this as a fallback.
     // Can execute pending proposal after liveness period has elapsed.
-    function executeAction(uint256 proposalId) external {
-        // Need to handle fact that livenesses can change after a proposal is pending...
-        uint256 livenessForProposal = permissionsTiers[permissionTierForFunctionSig[proposals[proposalId].functionSig]]
-            .liveness;
-        require(
-            block.timestamp >= proposals[proposalId].proposalTime + livenessForProposal,
-            "Cannot execute proposal until liveness period has elapsed."
-        );
-        proposals[nonce].status = ProposalStatus.Executed;
-        // Execute the action...
+    function executeAction(
+        address target,
+        bytes4 functionSig,
+        bytes memory callData,
+        uint256 msgValue
+    ) external {
+        if (roleForFunctionSig[functionSig] == bytes32(0)) {
+            // pass through, no role required
+        } else {
+            require(hasRole(roleForFunctionSig[bytes32(functionSig)], msg.sender), "invalid role for function sig.");
+
+            // execute functionSig with callData on target, optionally passing msgValue
+        }
     }
 }
