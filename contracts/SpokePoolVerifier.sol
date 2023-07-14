34d93c2f070184ea93294744198433daa629f92f
Matt Rice <matthewcrice32@gmail.com>
2023-07-14 12:03:59 -0400

    fix: [M01] add comment documenting that this contract will not protect zkSync (#313)
    * fix: [M01] add comment documenting that this contract will not protect zkSync
    
    * Update contracts/SpokePoolVerifier.sol
    
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>

--- contracts/SpokePoolVerifier.sol
+++ contracts/SpokePoolVerifier.sol
@@ -6,6 +6,10 @@ import "./interfaces/SpokePoolInterface.sol";
 
 /**
  * @notice SpokePoolVerifier is a contract that verifies that the SpokePool exists on this chain before sending ETH to it.
+ * @dev This contract must be deployed via Create2 to the same address on all chains. That way, an errant transaction sent
+ * to the wrong chain will be blocked by this contract rather than hitting a dead address. This means that this contract
+ * will not work to protect chains, like zkSync, where Create2 address derivations don't match other chains.
+ * Source: https://era.zksync.io/docs/reference/architecture/differences-with-ethereum.html#create-create2
  */
 contract SpokePoolVerifier {
     using Address for address;
