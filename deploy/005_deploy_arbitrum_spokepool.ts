b27549ca56ff2ad51e04f820cdbe2b2c29f2124f
Paul <108695806+pxrl@users.noreply.github.com>
2023-08-08 17:07:39 +0200

    fix: SpokePool deployment corrections (#342)
    The Ethereum SpokePool deployment script was unable to correctly resolve
    the HubPool address because it assumed that a companionNetworks entry
    was defined in the HardhatUserConfig in hardhat.config.ts. Resolve this
    by adjusting the Ethereum (HubPool) SpokePool deployment script to avoid
    using companionNetworks.
    
    Additionally, the other SpokePool deployment scripts incorrectly logged
    the chain ID for the HubPool deployment. This change now factors out the
    task of resolving the chain IDs and returns them in an unambiguous form.
    This also simplifies most of the deployment scripts.

--- deploy/005_deploy_arbitrum_spokepool.ts
+++ deploy/005_deploy_arbitrum_spokepool.ts
@@ -1,22 +1,20 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { L2_ADDRESS_MAP } from "./consts";
-import { deployNewProxy } from "../utils/utils.hre";
+import { deployNewProxy, getSpokePoolDeploymentInfo } from "../utils/utils.hre";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const hubPool = await hre.companionNetworks.l1.deployments.get("HubPool");
-  const chainId = await hre.getChainId();
-  console.log(`Using L1 (chainId ${chainId}) hub pool @ ${hubPool.address}`);
+  const { hubPool, spokeChainId } = await getSpokePoolDeploymentInfo(hre);
 
   // Initialize deposit counter to very high number of deposits to avoid duplicate deposit ID's
   // with deprecated spoke pool.
   // Set hub pool as cross domain admin since it delegatecalls the Adapter logic.
   const constructorArgs = [
     1_000_000,
-    L2_ADDRESS_MAP[chainId].l2GatewayRouter,
+    L2_ADDRESS_MAP[spokeChainId].l2GatewayRouter,
     hubPool.address,
     hubPool.address,
-    L2_ADDRESS_MAP[chainId].l2Weth,
+    L2_ADDRESS_MAP[spokeChainId].l2Weth,
   ];
   await deployNewProxy("Arbitrum_SpokePool", constructorArgs);
 };
