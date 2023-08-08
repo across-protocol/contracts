f434298ba41fa3a19d18223ab5423aebdb2153df
Paul <108695806+pxrl@users.noreply.github.com>
2023-08-08 16:46:25 +0200

    Leverage getSpokePoolDeploymentInfo helper

--- deploy/016_deploy_zksync_spokepool.ts
+++ deploy/016_deploy_zksync_spokepool.ts
@@ -3,14 +3,14 @@ import { Deployer as zkDeployer } from "@matterlabs/hardhat-zksync-deploy";
 import { DeployFunction, DeploymentSubmission } from "hardhat-deploy/types";
 import { L2_ADDRESS_MAP } from "./consts";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { getSpokePoolDeploymentInfo } from "../utils/utils.hre";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const contractName = "ZkSync_SpokePool";
-  const { companionNetworks, deployments, getChainId, zkUpgrades } = hre;
+  const { deployments, zkUpgrades } = hre;
 
-  const chainId = await getChainId();
-  const hubPool = await companionNetworks.l1.deployments.get("HubPool");
-  console.log(`Using HubPool @ ${hubPool.address}`);
+  const { hubPool, hubChainId, spokeChainId } = await getSpokePoolDeploymentInfo(hre);
+  console.log(`Using chain ${hubChainId} HubPool @ ${hubPool.address}`);
 
   const mnemonic = hre.network.config.accounts.mnemonic;
   const wallet = zk.Wallet.fromMnemonic(mnemonic);
@@ -19,10 +19,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const artifact = await deployer.loadArtifact(contractName);
   const initArgs = [
     0, // Start at 0 since this first time we're deploying this spoke pool. On future upgrades increase this.
-    L2_ADDRESS_MAP[chainId].zkErc20Bridge,
+    L2_ADDRESS_MAP[spokeChainId].zkErc20Bridge,
     hubPool.address,
     hubPool.address,
-    L2_ADDRESS_MAP[chainId].l2Weth,
+    L2_ADDRESS_MAP[spokeChainId].l2Weth,
   ];
 
   const proxy = await zkUpgrades.deployProxy(deployer.zkWallet, artifact, initArgs, {
@@ -32,7 +32,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
   console.log(`Deployment transaction hash: ${proxy.deployTransaction.hash}.`);
   await proxy.deployed();
-  console.log(`${contractName} deployed to chain ID ${chainId} @ ${proxy.address}.`);
+  console.log(`${contractName} deployed to chain ID ${spokeChainId} @ ${proxy.address}.`);
 
   // Save the deployment manually because OZ's hardhat-upgrades packages bypasses hardhat-deploy.
   // See also: https://stackoverflow.com/questions/74870472
