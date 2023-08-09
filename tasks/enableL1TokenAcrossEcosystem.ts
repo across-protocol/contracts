5af93f932c67300fe634fc764297b87c400bfbeb
nicholaspai <npai.nyc@gmail.com>
2023-08-09 10:23:50 -0400

    Update script

--- tasks/enableL1TokenAcrossEcosystem.ts
+++ tasks/enableL1TokenAcrossEcosystem.ts
@@ -1,6 +1,7 @@
 import { task } from "hardhat/config";
 import assert from "assert";
-import { findL2TokenForL1Token, askYesNoQuestion, zeroAddress, minimalSpokePoolInterface } from "./utils";
+import { askYesNoQuestion, minimalSpokePoolInterface } from "./utils";
+import { TOKEN_SYMBOLS_MAP } from "../utils/constants";
 
 const enabledChainIds = [1, 10, 137, 42161, 324]; // Supported mainnet chain IDs.
 
@@ -11,30 +12,27 @@ const getChainsFromList = (taskArgInput: string): number[] =>
     ?.map((chainId: string) => Number(chainId)) || [];
 
 task("enable-l1-token-across-ecosystem", "Enable a provided token across the entire ecosystem of supported chains")
-  .addParam("chain1token", "Address of the token to enable, as defined on L1")
   .addFlag("execute", "Provide this flag if you would like to actually execute the transaction from the EOA")
-  .addOptionalParam("chain10token", "Address of the token on chainID 10. Used to override the auto detect")
-  .addOptionalParam("chain137token", "Address of the token on chainID 137. Used to override the auto detect")
-  .addOptionalParam("chain42161token", "Address of the token on chainID 42161. Used to override the auto detect")
-  .addOptionalParam("chain324token", "Address of the token on chainID 324. Used to override the auto detect")
+  .addParam("token", "Symbol of token to enable")
+  .addParam("chains", "Comma-delimited list of chains to enable the token on. Defaults to all supported chains")
   .addOptionalParam(
     "customoptimismbridge",
     "Custom token bridge to set for optimism, for example used with SNX and DAI"
   )
-  .addOptionalParam("ignorechains", "ChainIds to ignore. Separated by comma.")
-  .addOptionalParam("exclusivechains", "ChainIds to enable exclusively. Separated by comma.")
+  .addOptionalParam("depositroutechains", "ChainIds to enable deposit routes for exclusively. Separated by comma.")
   .setAction(async function (taskArguments, hre_) {
     const hre = hre_ as any;
-    const l1Token = taskArguments.chain1token;
-    assert(l1Token, "chain1token argument must be provided");
-    const ignoredChainIds = getChainsFromList(taskArguments.ignorechains);
-    if (ignoredChainIds.includes(1)) throw new Error("Cannot ignore chainId 1");
+    const matchedSymbol = Object.keys(TOKEN_SYMBOLS_MAP).find(
+      (symbol) => symbol === taskArguments.token
+    ) as keyof typeof TOKEN_SYMBOLS_MAP;
+    assert(matchedSymbol !== undefined, `Could not find token with symbol ${taskArguments.token} in TOKEN_SYMBOLS_MAP`);
+    const l1Token = TOKEN_SYMBOLS_MAP[matchedSymbol];
 
-    // If exclusive chains are provided then we'll only add routes involving these chains. This is used to add new
+    // If deposit routes chains are provided then we'll only add routes involving these chains. This is used to add new
     // deposit routes to a new chain for an existing L1 token, so we also won't add a new LP token if this is defined.
-    const exclusiveChains = getChainsFromList(taskArguments.exclusivechains);
-    if (exclusiveChains.length > 0) {
-      console.log(`\n0. Only adding routes involving chains on exclusive list ${exclusiveChains.join(", ")}`);
+    const depositRouteChains = getChainsFromList(taskArguments.depositroutechains);
+    if (depositRouteChains.length > 0) {
+      console.log(`\n0. Only adding deposit routes involving chains on list ${depositRouteChains.join(", ")}`);
     }
 
     const hasSetConfigStore = await askYesNoQuestion(
@@ -47,42 +45,41 @@ task("enable-l1-token-across-ecosystem", "Enable a provided token across the ent
     const signer = (await hre.ethers.getSigners())[0];
 
     // Remove chainIds that are in the ignore list.
-    let chainIds = enabledChainIds.filter((chainId) => !ignoredChainIds.includes(chainId));
-
-    console.log("\n1. Auto detecting L2 companion token address for provided L1 token.");
-    const autoDetectedTokens = await Promise.all(
-      chainIds.slice(1).map((chainId) => findL2TokenForL1Token(chainId, l1Token))
-    );
-
-    let tokens: string[] = [];
-    tokens[0] = l1Token;
-    chainIds
-      .slice(1)
-      .forEach(
-        (chainId, index) => (tokens[index + 1] = taskArguments[`chain${chainId}token`] ?? autoDetectedTokens[index])
+    let inputChains: number[] = [];
+    try {
+      const parsedChains: string[] = taskArguments.chains.split(",");
+      inputChains = parsedChains.map((x) => Number(x));
+      console.log(`\nParsed 'chains' argument:`, inputChains);
+    } catch (error) {
+      throw new Error(
+        `Failed to parse 'chains' argument ${taskArguments.chains} as a comma-separated list of numbers.`
       );
-
-    for (let i = 0; i < chainIds.length; i++) {
-      const chainId = chainIds[i];
-      if (
-        tokens[i] === zeroAddress &&
-        !(await askYesNoQuestion(
-          `\nNo address found for chainId: ${chainId}. Would you like to remove routes to and from this chain?`
-        ))
-      ) {
-        console.log(`Please rerun with override address for chainId: ${chainId}`);
-        process.exit(0);
-      }
     }
+    if (inputChains.length === 0) inputChains = enabledChainIds;
+    else if (inputChains.some((chain) => isNaN(chain) || !Number.isInteger(chain) || chain < 0)) {
+      throw new Error(`Invalid chains list: ${inputChains}`);
+    }
+    const chainIds = enabledChainIds.filter((chainId) => inputChains.includes(chainId));
 
-    chainIds = chainIds.filter((_, index) => tokens[index] !== zeroAddress);
-    tokens = tokens.filter((token) => token !== zeroAddress);
+    console.log("\n1. Loading L2 companion token address for provided L1 token.");
+    const tokens = await Promise.all(
+      chainIds.map((chainId) => {
+        const l2Address = TOKEN_SYMBOLS_MAP[matchedSymbol].addresses[chainId];
+        if (l2Address === undefined) {
+          throw new Error(`Could not find token address on chain ${chainId} in TOKEN_SYMBOLS_MAP`);
+        }
+        return l2Address;
+      })
+    );
 
     console.table(
       chainIds.map((chainId, index) => {
-        return { chainId, address: tokens[index], autoDetected: taskArguments[`chain${chainId}token`] === undefined };
+        return {
+          chainId,
+          address: tokens[index],
+        };
       }),
-      ["chainId", "address", "autoDetected"]
+      ["chainId", "address"]
     );
 
     // Check the user is ok with the token addresses provided. If not, abort.
@@ -97,8 +94,8 @@ task("enable-l1-token-across-ecosystem", "Enable a provided token across the ent
     // Construct calldata to enable these tokens.
     const callData = [];
 
-    // If exclusive chain is defined then we don't want to add a new LP token:
-    if (exclusiveChains.length > 0) {
+    // If deposit route chains are defined then we don't want to add a new LP token:
+    if (depositRouteChains.length === 0) {
       console.log("\n5. Adding calldata to enable liquidity provision on", l1Token);
       callData.push(hubPool.interface.encodeFunctionData("enableL1TokenForLiquidityProvision", [l1Token]));
     }
@@ -109,8 +106,12 @@ task("enable-l1-token-across-ecosystem", "Enable a provided token across the ent
       chainIds.forEach((toId, _) => {
         if (fromId === toId) return;
 
-        // If exclusive chains is defined, only add route if it involves a chain on that list
-        if (exclusiveChains.length === 0 || exclusiveChains.includes(toId) || exclusiveChains.includes(fromId)) {
+        // If deposit route chains are defined, only add route if it involves a chain on that list
+        if (
+          depositRouteChains.length === 0 ||
+          depositRouteChains.includes(toId) ||
+          depositRouteChains.includes(fromId)
+        ) {
           console.log(`\t 6.${++i}\t Adding calldata for token ${tokens[fromIndex]} for route ${fromId} -> ${toId}`);
           callData.push(
             hubPool.interface.encodeFunctionData("setDepositRoute", [fromId, toId, tokens[fromIndex], true])
@@ -123,46 +124,60 @@ task("enable-l1-token-across-ecosystem", "Enable a provided token across the ent
       });
     });
 
-    console.log("\n7. Adding calldata to set the pool rebalance route for the respective destination tokens:");
-    let j = 0; // counter for logging.
-    chainIds.forEach((toId, toIndex) => {
-      // If exclusive chains is defined, only add route if it involves a chain on that list
-      if (exclusiveChains.length === 0 || exclusiveChains.includes(toId)) {
-        console.log(`\t 7.${++j}\t Adding calldata for rebalance route for L2Token ${tokens[toIndex]} on ${toId}`);
-        callData.push(hubPool.interface.encodeFunctionData("setPoolRebalanceRoute", [toId, l1Token, tokens[toIndex]]));
-      } else {
+    // If deposit route chains are defined then we don't want to add a new PoolRebalanceRoute
+    if (depositRouteChains.length === 0) {
+      console.log("\n7. Adding calldata to set the pool rebalance route for the respective destination tokens:");
+      let j = 0; // counter for logging.
+      chainIds.forEach((toId, toIndex) => {
+        // If deposit route chains are defined, only add route if it involves a chain on that list
+        if (depositRouteChains.length === 0 || depositRouteChains.includes(toId)) {
+          console.log(`\t 7.${++j}\t Adding calldata for rebalance route for L2Token ${tokens[toIndex]} on ${toId}`);
+          callData.push(
+            hubPool.interface.encodeFunctionData("setPoolRebalanceRoute", [toId, l1Token, tokens[toIndex]])
+          );
+        } else {
+          console.log(
+            `\t\t Skipping pool rebalance rout -> ${toId} because it doesn't involve a chain on the exclusive list`
+          );
+        }
+      });
+
+      // We only need to whitelist an Arbitrum token on the SpokePool if we're setting up a pool rebalance route between
+      // mainnet and Arbitrum, so if deposit route chains are set then no need to do this.
+      if (chainIds.includes(42161)) {
+        const arbitrumToken = tokens[chainIds.indexOf(42161)];
         console.log(
-          `\t\t Skipping pool rebalance rout -> ${toId} because it doesn't involve a chain on the exclusive list`
+          `\n8. Adding call data to whitelist L2 ${arbitrumToken} -> L1 token ${l1Token} on Arbitrum. This is only needed on this chain`
+        );
+
+        // Address doesn't matter, we only want the interface.
+        const spokePool = new ethers.Contract(hubPoolDeployment.address, minimalSpokePoolInterface, signer);
+        // Find the address of the the Arbitrum representation of this token. Construct whitelistToken call to send to the
+        // Arbitrum spoke pool via the relaySpokeAdminFunction call.
+        const whitelistTokenCallData = spokePool.interface.encodeFunctionData("whitelistToken", [
+          arbitrumToken,
+          l1Token,
+        ]);
+        callData.push(
+          hubPool.interface.encodeFunctionData("relaySpokePoolAdminFunction", [42161, whitelistTokenCallData])
         );
       }
-    });
 
-    if (chainIds.includes(42161)) {
-      console.log("\n8. Adding call data to whitelist L1 token on Arbitrum. This is only needed on this chain");
+      // Add optimism setTokenBridge call if the token has a custom bridge needed to get to mainnet.
+      if (chainIds.includes(10) && taskArguments.customoptimismbridge) {
+        console.log("\n9. Adding call data to set custom Optimism bridge.");
 
-      // Address doesn't matter, we only want the interface.
-      const spokePool = new ethers.Contract(hubPoolDeployment.address, minimalSpokePoolInterface, signer);
-      // Find the address of the the Arbitrum representation of this token. Construct whitelistToken call to send to the
-      // Arbitrum spoke pool via the relaySpokeAdminFunction call.
-      const arbitrumToken = tokens[chainIds.indexOf(42161)];
-      const whitelistTokenCallData = spokePool.interface.encodeFunctionData("whitelistToken", [arbitrumToken, l1Token]);
-      callData.push(
-        hubPool.interface.encodeFunctionData("relaySpokePoolAdminFunction", [42161, whitelistTokenCallData])
-      );
-    }
-
-    // Add optimism setTokenBridge call
-    if (chainIds.includes(10) && taskArguments.customoptimismbridge) {
-      console.log("\n9. Adding call data to set custom Optimism bridge.");
-
-      // Address doesn't matter, we only want the interface:
-      const spokePool = new ethers.Contract(hubPoolDeployment.address, minimalSpokePoolInterface, signer);
-      const optimismToken = tokens[chainIds.indexOf(10)];
-      const setTokenBridgeCallData = spokePool.interface.encodeFunctionData("setTokenBridge", [
-        optimismToken,
-        taskArguments.customoptimismbridge,
-      ]);
-      callData.push(hubPool.interface.encodeFunctionData("relaySpokePoolAdminFunction", [10, setTokenBridgeCallData]));
+        // Address doesn't matter, we only want the interface:
+        const spokePool = new ethers.Contract(hubPoolDeployment.address, minimalSpokePoolInterface, signer);
+        const optimismToken = tokens[chainIds.indexOf(10)];
+        const setTokenBridgeCallData = spokePool.interface.encodeFunctionData("setTokenBridge", [
+          optimismToken,
+          taskArguments.customoptimismbridge,
+        ]);
+        callData.push(
+          hubPool.interface.encodeFunctionData("relaySpokePoolAdminFunction", [10, setTokenBridgeCallData])
+        );
+      }
     }
 
     console.log(`\n10. ***DONE.***\nCalldata to enable desired token has been constructed!`);
