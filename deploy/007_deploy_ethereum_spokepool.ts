6dbaa61519f1e299de9757ceec93b67ec7ee7615
james-a-morris <jaamorris@cs.stonybrook.edu>
2023-12-19 13:58:42 -0500

    fix: command args

--- deploy/007_deploy_ethereum_spokepool.ts
+++ deploy/007_deploy_ethereum_spokepool.ts
@@ -11,8 +11,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   // Initialize deposit counter to very high number of deposits to avoid duplicate deposit ID's
   // with deprecated spoke pool.
-  const initArgs = [1_000_000, hubPool.address, L1_ADDRESS_MAP[chainId].weth];
-  await deployNewProxy("Ethereum_SpokePool", [L1_ADDRESS_MAP[chainId].weth, 3600, 32400], initArgs);
+  const initArgs = [1_000_000, hubPool.address];
+  const constructorArgs = [L1_ADDRESS_MAP[chainId].weth, 3600, 32400];
+  await deployNewProxy("Ethereum_SpokePool", constructorArgs, initArgs);
 
   // Transfer ownership to hub pool.
 };
