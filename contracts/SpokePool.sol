af8b14fc15ce735b4de73b656a9d2a8458fa985a
nicholaspai <npai.nyc@gmail.com>
2024-01-05 16:11:07 -0500

    fix(SpokePool): Decrement __gap due to new fillStatuses mapping
    https://github.com/across-protocol/contracts-v2/pull/379/files added this new public mapping and we forgot to decrement the storage gap

--- contracts/SpokePool.sol
+++ contracts/SpokePool.sol
@@ -1806,5 +1806,5 @@ abstract contract SpokePool is
     // Reserve storage slots for future versions of this base contract to add state variables without
     // affecting the storage layout of child contracts. Decrement the size of __gap whenever state variables
     // are added. This is at bottom of contract to make sure it's always at the end of storage.
-    uint256[1000] private __gap;
+    uint256[999] private __gap;
 }
