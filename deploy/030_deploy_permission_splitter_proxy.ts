08834ed1c94b1258e1dec4af30336efdd37cd91a
nicholaspai <npai.nyc@gmail.com>
2024-01-25 11:03:46 -0500

    feat: Deploy PermissionSplitter via forge
    adds forge deployment script and other setup along with newly deployed contract at [0x0Bf07B2e415F02711fFBB32491f8ec9e5489B2e7](https://etherscan.io/address/0x0Bf07B2e415F02711fFBB32491f8ec9e5489B2e7#code)

---
+++ deploy/030_deploy_permission_splitter_proxy.ts
@@ -0,0 +1,22 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  // Deploy from EOA for convenience
+  // Grant default admin role to new admin
+  // revoke role from deployer
+  await deploy("PermissionSplitterProxy", {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+    args: [],
+  });
+};
+
+module.exports = func;
+func.tags = ["MintableERC1155"];
