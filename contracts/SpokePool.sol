4f0b39f982d68641d5e6ed09a3f1603b82b6085d
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-02-07 18:29:56 -0500

    improve(SpokePool): Delete chainId in ExecutedV3RelayerRefundRoot event
    chainId is not useful because it is known based on the SpokePool being
    queried. Also, the marginal value of indexing the leafId on top of the
    rootBundleId is pretty minimal, so instead index fillsRefundedRoot.

--- contracts/SpokePool.sol
+++ contracts/SpokePool.sol
@@ -1345,7 +1345,6 @@ abstract contract SpokePool is
 
         emit ExecutedV3RelayerRefundRoot(
             relayerRefundLeaf.amountToReturn,
-            relayerRefundLeaf.chainId,
             relayerRefundLeaf.refundAmounts,
             rootBundleId,
             relayerRefundLeaf.leafId,
