d010cd868b984ccd933fb06b04e29b5d45d414fa
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-02-09 15:15:35 -0500

    feat: Update MockSpokePool to include a depositV2 test function
    * feat: Add depositV2 to MockSpokePool to support relayer-v2/sdk-v2
    unit tests
    
    These unit tests expect to query FundsDeposited events so we should add
    a function to the MockSpokePool so that they can access these older
    events using functions exported out of dist/test-utils

--- contracts/test/MockSpokePool.sol
+++ contracts/test/MockSpokePool.sol
@@ -11,6 +11,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract MockSpokePool is SpokePool, OwnableUpgradeable {
     uint256 private chainId_;
     uint256 private currentTime;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
 
     event BridgedToHubPool(uint256 amount, address token);
     event PreLeafExecuteHook(address token);
@@ -104,5 +105,64 @@ contract MockSpokePool is SpokePool, OwnableUpgradeable {
 
     function setChainId(uint256 _chainId) public {
         chainId_ = _chainId;
+    }
+
+    // Use this function to unit test that relayer-v2 and sdk-v2 clients can handle FundsDeposited events while also
+    // handling the new V3 events. This function is not explicitly tested in this repository but this contract is
+    // exported and tested in relayer-v2 and sdk-v2 by clients that do contain logic to handle these deprecated
+    // V2 events. After the V3 migration has taken place and there are no more FundsDeposited events queried by
+    // the dataworker and relayer, this function can be deprecated and the V2 unit tests can be removed from
+    // relayer-v2 and sdk-v2.
+    function depositV2(
+        address recipient,
+        address originToken,
+        uint256 amount,
+        uint256 destinationChainId,
+        int64 relayerFeePct,
+        uint32 quoteTimestamp,
+        bytes memory message,
+        uint256 // maxCount
+    ) public payable nonReentrant unpausedDeposits {
+        // Check that deposit route is enabled.
+        require(enabledDepositRoutes[originToken][destinationChainId], "Disabled route");
+
+        // We limit the relay fees to prevent the user spending all their funds on fees.
+        require(SignedMath.abs(relayerFeePct) < 0.5e18, "Invalid relayer fee");
+        require(amount <= MAX_TRANSFER_SIZE, "Amount too large");
+
+        // Require that quoteTimestamp has a maximum age so that depositors pay an LP fee based on recent HubPool usage.
+        // It is assumed that cross-chain timestamps are normally loosely in-sync, but clock drift can occur. If the
+        // SpokePool time stalls or lags significantly, it is still possible to make deposits by setting quoteTimestamp
+        // within the configured buffer. The owner should pause deposits if this is undesirable. This will underflow if
+        // quoteTimestamp is more than depositQuoteTimeBuffer; this is safe but will throw an unintuitive error.
+
+        // slither-disable-next-line timestamp
+        require(getCurrentTime() - quoteTimestamp <= depositQuoteTimeBuffer, "invalid quoteTimestamp");
+
+        // Increment count of deposits so that deposit ID for this spoke pool is unique.
+        uint32 newDepositId = numberOfDeposits++;
+
+        // If the address of the origin token is a wrappedNativeToken contract and there is a msg.value with the
+        // transaction then the user is sending ETH. In this case, the ETH should be deposited to wrappedNativeToken.
+        if (originToken == address(wrappedNativeToken) && msg.value > 0) {
+            require(msg.value == amount, "msg.value must match amount");
+            wrappedNativeToken.deposit{ value: msg.value }();
+            // Else, it is a normal ERC20. In this case pull the token from the user's wallet as per normal.
+            // Note: this includes the case where the L2 user has WETH (already wrapped ETH) and wants to bridge them.
+            // In this case the msg.value will be set to 0, indicating a "normal" ERC20 bridging action.
+        } else IERC20Upgradeable(originToken).safeTransferFrom(msg.sender, address(this), amount);
+
+        emit FundsDeposited(
+            amount,
+            chainId(),
+            destinationChainId,
+            relayerFeePct,
+            newDepositId,
+            quoteTimestamp,
+            originToken,
+            recipient,
+            msg.sender,
+            message
+        );
     }
 }
