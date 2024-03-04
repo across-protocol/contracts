2efcab8e528cdabae8eca267ab524382063f3b2c
nicholaspai <npai.nyc@gmail.com>
2024-03-04 09:26:19 -0700

    Update storageLayout.sh

--- scripts/storageLayout.sh
+++ scripts/storageLayout.sh
@@ -10,15 +10,24 @@ fi
 echo "Creating a storage layout snapshot of the $CONTRACT contract at ./storage-layouts/proposed.$CONTRACT.json"
 echo "⏳ This can take a few minutes to complete if you haven't run a forge command recently"
 touch ./storage-layouts/proposed.$CONTRACT.json
-forge inspect $CONTRACT storagelayout > ./storage-layouts/proposed.$CONTRACT.json
-echo "✅ Done!"
 
-read -r -p "Do you want to run a diff on the proposed storage layout with the deployed one? [y/N] " response
+read -r -p "Do you want to overwrite $CONTRACT.json? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-        vim -d ./storage-layouts/proposed.$CONTRACT.json ./storage-layouts/$CONTRACT.json
+        forge inspect $CONTRACT storagelayout > ./storage-layouts/$CONTRACT.json
+        echo "✅ Done!"
         ;;
     *)
+        forge inspect $CONTRACT storagelayout > ./storage-layouts/proposed.$CONTRACT.json
+        echo "✅ Done!"
+        read -r -p "Do you want to run a diff on the proposed storage layout with the deployed one? [y/N] " response
+        case "$response" in
+            [yY][eE][sS]|[yY]) 
+                vim -d ./storage-layouts/proposed.$CONTRACT.json ./storage-layouts/$CONTRACT.json
+                ;;
+            *)
+                ;;
+        esac
         ;;
 esac
 
