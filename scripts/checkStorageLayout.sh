6b6cf8f3a48d3bc2bf3c151bb51bc02b79d113fa
nicholaspai <npai.nyc@gmail.com>
2024-03-26 18:51:45 -0400

    WIP

--- scripts/checkStorageLayout.sh
+++ scripts/checkStorageLayout.sh
@@ -12,13 +12,9 @@ fi
 
 for CONTRACT in "${CONTRACTS[@]}"; do
     echo "Comparing storage layout snapshot of the $CONTRACT contract at ./storage-layouts/$CONTRACT.json with current storage layout"
-    touch ./storage-layouts/proposed.$CONTRACT.json
     echo "Created temporary storage layout file at ./storage-layouts/proposed.$CONTRACT.json"
     forge inspect $CONTRACT storagelayout > ./storage-layouts/proposed.$CONTRACT.json
     echo "'forge inspect' command created temp storage layout file!"
-    ## TODO: add an automatic check here like, if there is diff, throw error. This forces
-    ## the developer to include the updated JSON file in the storage-layouts/ directory
-    ## in their commit, assuming this script is running in CI.
     if ! diff -q "./storage-layouts/proposed.$CONTRACT.json" "./storage-layouts/$CONTRACT.json" &>/dev/null; then
         >&2 echo "❌ Diff detected in storage layout for $CONTRACT. Please update the storage layout file in the storage-layouts/ directory."
         echo "You can generate a new storage layout file by running this script with the '--overwrite' flag"
--- scripts/diffStorageLayout.sh
+++ scripts/diffStorageLayout.sh
@@ -9,12 +9,9 @@ fi
 
 echo "Creating a storage layout snapshot of the $CONTRACT contract at ./storage-layouts/proposed.$CONTRACT.json"
 echo "⏳ This can take a few minutes to complete if you haven't run a forge command recently"
-touch ./storage-layouts/proposed.$CONTRACT.json
-
 forge inspect $CONTRACT storagelayout > ./storage-layouts/proposed.$CONTRACT.json
 echo "✅ Done!"
 vim -d ./storage-layouts/proposed.$CONTRACT.json ./storage-layouts/$CONTRACT.json
-;;
 
 exit 0
 
