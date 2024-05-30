84064a9de805d5719a42716dfa5f2e66bfcb7d40
Matt Rice <matthewcrice32@gmail.com>
2024-05-30 17:04:23 -0400

    fix: remove all v2/v3 references
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- test-utils.ts
+++ test-utils.ts
@@ -1,8 +1,8 @@
 // Note: this file sits on a separate export path and is intended to export test utilities and code:
-// You can import it like this: import * as testUtils from "@across-protocol/contracts-v2/dist/test-utils".
+// You can import it like this: import * as testUtils from "@across-protocol/contracts/dist/test-utils".
 // This is separated because this code assumes the caller has a hardhat config because it imports
 // hardhat. For non-test code, import the standard index file:
-// import * as contracts-v2 from "@across-protocol/contracts-v2"
+// import * as contracts from "@across-protocol/contracts"
 export * from "./test/fixtures/SpokePool.Fixture";
 export * from "./test/fixtures/HubPool.Fixture";
 export * from "./test/fixtures/MerkleLib.Fixture";
