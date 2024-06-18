23b20950c1b036e0f22fc0e7a4fe906634ddea27
nicholaspai <npai.nyc@gmail.com>
2024-06-18 09:01:22 -0400

    fix imports

--- contracts/erc7683/ERC7683OrderDepositor.sol
+++ contracts/erc7683/ERC7683OrderDepositor.sol
@@ -2,10 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "../external/interfaces/IPermit2.sol";
-import "../interfaces/V3SpokePoolInterface.sol";
-
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { Input, Output, CrossChainOrder, ResolvedCrossChainOrder, ISettlementContract } from "./ERC7683.sol";
--- contracts/erc7683/ERC7683OrderDepositorExternal.sol
+++ contracts/erc7683/ERC7683OrderDepositorExternal.sol
@@ -1,4 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { ERC7683OrderDepositor } from "./ERC7683OrderDepositor.sol";
+import "../interfaces/V3SpokePoolInterface.sol";
+import "../external/interfaces/IPermit2.sol";
 
 /**
  * @notice ERC7683OrderDepositorExternal processes an external order type and translates it into an AcrossV3Deposit
