59936e9e589758477a61f6d58b4b78c8dadb30ba
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-06-24 14:41:22 -0400

    feat: Deploy pre-audit versions of MulticallHandler (#533)
    * feat: Initial MulticallHandler deployments
    
    * feat: Deploy pre-audit versions of MulticallHandler
    
    These are likely to match post-audit versions but are marked pre-audit officially

---
+++ deploy/040_deploy_multicall_handler.ts
@@ -0,0 +1,24 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  // @note if deploying this contract on a chain like Linea that only supports up to
+  // solc 0.8.19, the hardhat.config solc version needs to be overridden and this
+  // contract needs to be recompiled.
+  await deploy("Multicallhandler", {
+    contract: "MulticallHandler",
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+    args: [],
+    deterministicDeployment: "0x12345678", // Salt for the create2 call. This will deploy this contract
+    // at the same address on all chains except ZkSync and Linea which are not EVM equivalent.
+  });
+};
+module.exports = func;
+func.tags = ["Multicallhandler"];
