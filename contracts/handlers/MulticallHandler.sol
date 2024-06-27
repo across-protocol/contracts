50bc69eb7c2529cb4d1b8d898c4715ba993013ae
Paul <108695806+pxrl@users.noreply.github.com>
2024-06-27 23:04:48 +0200

    refactor: Relocate message handler interface
    The MulticallHandler contract currently imports the SpokePool contract
    and experienced a bytecode change after the following unrelated change:
    
    86593c6
    
    This change resulted in its CREATE2 deployment address changing. The
    MulticallHandler doesn't actually need any of the SpokePool
    implementation or its dependencies, and it seems like we'd like the
    MulticallHandler to be deployed to the same address on as many chains as
    possible, so factor out the interface to a separate file to insulate it
    from any unintended changes.

--- contracts/handlers/MulticallHandler.sol
+++ contracts/handlers/MulticallHandler.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "../SpokePool.sol";
+import "../interfaces/SpokePoolMessageHandler.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
