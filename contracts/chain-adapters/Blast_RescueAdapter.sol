882855b8d69758457b3e3bbdd0899141194da7ce
nicholaspai <npai.nyc@gmail.com>
2024-07-24 15:56:46 -0400

    Update Blast_RescueAdapter.sol

--- contracts/chain-adapters/Blast_RescueAdapter.sol
+++ contracts/chain-adapters/Blast_RescueAdapter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "./interfaces/AdapterInterface.sol";
-import { USDYieldManager } from "../BlastTokenBridger.sol";
+import { USDYieldManager } from "../Blast_DaiRetriever.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
