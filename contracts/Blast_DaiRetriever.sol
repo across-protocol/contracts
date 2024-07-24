90b2d436da09f2a60ac1f72cb83663deac6f577d
nicholaspai <npai.nyc@gmail.com>
2024-07-24 15:11:00 -0400

    feat(Blast): Add Blast_DaiRetriever and Blast_RescueAdapter
    We recently found out that USDB L2 to L1 withdrawals are a two step process:
    - L2 withdrawal is requested, passes challenge period, and finalized like a normal OPStack withdrawal on the OptimismPortal
    - [This function finalizeBridgeErc20](https://github.com/blast-io/blast/blob/706acdde2b3a6614a9a7dbfceb82660789d5e7b8/blast-optimism/packages/contracts-bedrock/src/mainnet-bridge/L1BlastBridge.sol#L187-L210) is called on the L1BlastBridge which calls to the [USDYieldManager](https://etherscan.io/address/0xa230285d5683C74935aD14c446e137c8c8828438) to request the withdrawal of DAI
    - This final request needs to be finalized via an admin call which happens every ~12 hours. Once finalized, the original withdrawal's `recipient` can call `claimWithdrawal` on the `USDYieldManager`.
    
    Therefore, this PR adds a Blast_DaiRetriever which should be set as the `recipient` on all Blast withdrawals from the SpokePool going forwards. An EOA can then call `retrieve` with the L1 finalized withdrawal's `requestId` to retrieve DAI following the 12 hour window.
    
    Separately, the `Blast_RescueAdapter` can be used to retrieve any DAI currently available in the USDYieldManager that must be called by the `HubPool`

---
+++ contracts/Blast_DaiRetriever.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "./Lockable.sol";
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+interface USDYieldManager {
+    function claimWithdrawal(uint256 _requestId, uint256 _hintId) external returns (bool success);
+
+    function findCheckpointHint(
+        uint256 _requestId,
+        uint256 _start,
+        uint256 _end
+    ) external view returns (uint256);
+
+    function getLastCheckpointId() external view returns (uint256);
+}
+
+/**
+ * @notice Contract deployed on Ethereum to facilitate DAI transfers from Blast to the HubPool.
+ * @dev Blast USDB withdrawals are a two step process where the L2 to L1 withdrawal must first be finalized via
+ * the typical OP Stack mechanism, and then a claim from the withdrawal's *recipient* must be made against a
+ * USDBYieldManager contract. This means that the Blast_SpokePool must set its recipient to this contract's address
+ * and then an EOA can call this contract to retrieve the DAI.
+ */
+contract Blast_DaiRetriever is Lockable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // Should be set to HubPool on Ethereum
+    address public immutable hubPool;
+
+    // USDCYieldManager contract on Ethereum which releases DAI to the hubPool.
+    USDYieldManager public immutable usdYieldManager;
+
+    // Token to be retrieved.
+    IERC20Upgradeable public immutable dai;
+
+    /**
+     * @notice Constructs USDB Retriever
+     * @param _hubPool Where to send DAI to.
+     * @param _usdYieldManager USDCYieldManager contract on Ethereum.
+     * @param _dai DAI token to be retrieved.
+     */
+    constructor(
+        address _hubPool,
+        USDYieldManager _usdYieldManager,
+        IERC20Upgradeable _dai
+    ) {
+        //slither-disable-next-line missing-zero-check
+        hubPool = _hubPool;
+        usdYieldManager = _usdYieldManager;
+        dai = _dai;
+    }
+
+    /**
+     * @notice Calls USDCYieldManager contract to release DAI and send to the hubPool. Required to use this function
+     * to retrieve DAI since only the L2 withdrawal recipient can make this call.
+     * @notice This can revert if the claim is not ready yet. It takes ~12 hours for a Blast admin to make the DAI
+     * available for retrieval following withdrawal finalization.
+     * @param _requestId L2 withdrawal request ID. Emitted in L1 WithdrawalRequested event when the L2 to L1
+     * withdrawal is first "finalized" but still awaiting the recipient to claim the DAI.
+     */
+    function retrieve(uint256 _requestId) public nonReentrant {
+        uint256 hintId = usdYieldManager.findCheckpointHint(_requestId, 1, usdYieldManager.getLastCheckpointId());
+        require(usdYieldManager.claimWithdrawal(_requestId, hintId), "claim failed");
+        dai.safeTransfer(hubPool, dai.balanceOf(address(this)));
+    }
+}
---
+++ contracts/chain-adapters/Blast_RescueAdapter.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "./interfaces/AdapterInterface.sol";
+import { USDYieldManager } from "../BlastTokenBridger.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @notice This adapter is built to to retrieve Blast USDB from the USDBYieldManager contract on Ethereum that was
+ * sent to the HubPool as the `recipient`. These funds should ideally be sent to the BlastRetriever address on
+ * Ethereum. This contract can be used to retrieve these funds.
+ */
+// solhint-disable-next-line contract-name-camelcase
+contract Blast_RescueAdapter is AdapterInterface {
+    using SafeERC20 for IERC20;
+
+    address public immutable rescueAddress;
+
+    USDYieldManager public immutable usdYieldManager;
+
+    /**
+     * @notice Constructs new Adapter.
+     * @param _rescueAddress Rescue address to send funds to.
+     */
+    constructor(address _rescueAddress, USDYieldManager _usdYieldManager) {
+        rescueAddress = _rescueAddress;
+        usdYieldManager = _usdYieldManager;
+    }
+
+    /**
+     * @notice Rescues the tokens from the calling contract.
+     * @param message The encoded address of the ERC20 to send to the rescue addres.
+     */
+    function relayMessage(address, bytes memory message) external payable override {
+        (uint256 requestId, uint256 hintId) = abi.decode(message, (uint256, uint256));
+        require(usdYieldManager.claimWithdrawal(requestId, hintId), "claim failed");
+    }
+
+    /**
+     * @notice Should never be called.
+     */
+    function relayTokens(
+        address,
+        address,
+        uint256,
+        address
+    ) external payable override {
+        revert("relayTokens disabled");
+    }
+}
