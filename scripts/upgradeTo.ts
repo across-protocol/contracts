e046ec2a413aa8e506366bd8fe81c525d21f3e1e
nicholaspai <npai.nyc@gmail.com>
2024-07-24 08:10:11 -0400

    update script
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- scripts/upgradeTo.ts
+++ scripts/upgradeTo.ts
@@ -1,14 +1,22 @@
 import { ethers, getContractFactory } from "../utils/utils";
 
 async function main() {
+  if (!process.env.NEW_SPOKE_POOL) {
+    console.log("Usage: NEW_SPOKE_POOL=<NEW_SPOKE_POOL_ADDRESS> yarn hardhat run scripts/upgradeTo.ts");
+    return;
+  }
+  // @dev This should throw if the address is invalid.
+  const newSpokePoolAddress = ethers.utils.getAddress(process.env.NEW_SPOKE_POOL);
   const [signer] = await ethers.getSigners();
 
+  // @dev Any spoke pool's interface can be used here since they all should have the same upgradeTo function signature.
   const spokePool = await getContractFactory("Arbitrum_SpokePool", { signer });
-  const upgradeTo = spokePool.interface.encodeFunctionData("upgradeTo", ["0xe1601d869F3C72FDf12B9F40Ca18e8c6c5f5D860"]);
+
+  const upgradeTo = spokePool.interface.encodeFunctionData("upgradeTo", [newSpokePoolAddress]);
   console.log(`upgradeTo bytes: `, upgradeTo);
 
   console.log(
-    `Call relaySpokePoolAdminFunction() with the params [<chainId>, ${upgradeTo}] on this hub pool: https://goerli.etherscan.io/address/0x0e2817C49698cc0874204AeDf7c72Be2Bb7fCD5d#writeContract`
+    `Call relaySpokePoolAdminFunction() with the params [<chainId>, ${upgradeTo}] on the hub pool from the owner's account.`
   );
 }
 
