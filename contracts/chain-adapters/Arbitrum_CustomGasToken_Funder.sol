9c36dcd7526613a48d0fd50d2c2e3f7a68cebf9e
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2024-09-03 17:48:34 -0400

    improve(Arbitrum_CustomGasToken_Funder): Add better comments about the withdraw() use case

--- contracts/chain-adapters/Arbitrum_CustomGasToken_Funder.sol
+++ contracts/chain-adapters/Arbitrum_CustomGasToken_Funder.sol
@@ -10,6 +10,10 @@ contract Arbitrum_CustomGasToken_Funder is Ownable {
 
     /**
      * @notice Withdraw tokens from the contract.
+     * @dev Only callable by owner, which should be set to the HubPool
+     * so that it can use these funds to pay for relaying messages to
+     * an Arbitrum L2 that uses custom gas tokens as the L1 payment currency,
+     * via the Arbitrum_CustomGasToken_Adapter.
      * @param token Token to withdraw.
      * @param amount Amount of tokens to withdraw.
      */
