0242dcddf23fb3d12f6d1250dc4d9bdae67f2b3c
nicholaspai <npai.nyc@gmail.com>
2024-09-06 10:20:39 -0400

    improve: Use explicit imports in Arbitrum_CustomGasToken_Adapter

--- contracts/chain-adapters/Arbitrum_CustomGasToken_Adapter.sol
+++ contracts/chain-adapters/Arbitrum_CustomGasToken_Adapter.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import "./interfaces/AdapterInterface.sol";
+import { AdapterInterface } from "./interfaces/AdapterInterface.sol";
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "../external/interfaces/CCTPInterfaces.sol";
-import "../libraries/CircleCCTPAdapter.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { CCTPInterfaces } from "../external/interfaces/CCTPInterfaces.sol";
+import { CircleCCTPAdapter } from "../libraries/CircleCCTPAdapter.sol";
 
 interface FunderInterface {
     function withdraw(IERC20 token, uint256 amount) external;
