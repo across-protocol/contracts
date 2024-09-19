dea96a431b9c68c0e57b4a123d12d0387308d3a7
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2024-09-19 13:02:58 +0200

    refactor(svm-utils): split utility functions into separate files

--- programs/svm-spoke/src/instructions/slow_fill.rs
+++ programs/svm-spoke/src/instructions/slow_fill.rs
@@ -1,7 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::keccak;
 
-use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token_interface::{
     transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
 };
