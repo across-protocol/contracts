9f48b4226bf09119d3d390670651a3b0a44b6348
Chris Maree <christopher.maree@gmail.com>
2024-09-20 14:41:56 +0800

    Add initial SVM-Spoke MVP from closed repo (#611)
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * WIP
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    
    * Update Anchor.toml
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/closeRelayerPdas.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/remotePauseDeposits.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/initialize.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/enableRoute.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/queryState.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/queryDeposits.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/remotePauseDeposits.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/queryFills.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/simpleDeposit.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/simpleFill.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/queryRoute.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    * Update scripts/svm/remotePauseDeposits.ts
    
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: chrismaree <christopher.maree@gmail.com>
    Co-authored-by: Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>

---
+++ programs/svm-spoke/src/instructions/mod.rs
@@ -0,0 +1,17 @@
+mod admin;
+mod bundle;
+mod deposit;
+mod fill;
+mod handle_receive_message;
+mod slow_fill;
+mod testable;
+mod token_bridge;
+
+pub use admin::*;
+pub use bundle::*;
+pub use deposit::*;
+pub use fill::*;
+pub use handle_receive_message::*;
+pub use slow_fill::*;
+pub use testable::*;
+pub use token_bridge::*;
