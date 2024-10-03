a054134be2aa5bd825d22fd6cfeb1d9437ed7274
Matt Rice <matthewcrice32@gmail.com>
2024-10-03 06:58:06 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- contracts/chain-adapters/ZkStack_Adapter.sol
+++ contracts/chain-adapters/ZkStack_Adapter.sol
@@ -87,22 +87,7 @@ interface BridgeHubInterface {
 }
 
 /**
- * @notice Interface for funder contract that this contract pulls from to pay for relayMessage()/relayTokens()
- * fees using a custom gas token.
- */
-interface FunderInterface {
-    /**
-     * @notice Withdraws amount of token from funder contract to the caller.
-     * @dev Can only be called by owner of Funder contract, which therefore must be
-     * this contract.
-     * @param token Token to withdraw.
-     * @param amount Amount to withdraw.
-     */
-    function withdraw(IERC20 token, uint256 amount) external;
-}
-
-/**
- * @notice Contract containing logic to send messages from L1 to ZkStack with a custom gas token.
+ * @notice Contract containing logic to send messages from L1 to ZkStack with ETH as the gas token.
  * @dev Public functions calling external contracts do not guard against reentrancy because they are expected to be
  * called via delegatecall, which will execute this contract's logic within the context of the originating contract.
  * For example, the HubPool will delegatecall these functions, therefore its only necessary that the HubPool's methods
@@ -111,7 +96,7 @@ interface FunderInterface {
  */
 
 // solhint-disable-next-line contract-name-camelcase
-contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
+contract ZkStack_Adapter is AdapterInterface {
     using SafeERC20 for IERC20;
 
     // We need to pay a base fee to the operator to include our L1 --> L2 transaction.
@@ -142,14 +127,8 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
     // SharedBridge address, which is read from the BridgeHub at construction.
     address public immutable SHARED_BRIDGE;
 
-    // Custom gas token address, which is read from the BridgeHub at construction.
-    address public immutable CUSTOM_GAS_TOKEN;
-
-    // Custom gas token funder
-    FunderInterface public immutable CUSTOM_GAS_TOKEN_FUNDER;
-
     event ZkStackMessageRelayed(bytes32 canonicalTxHash);
-    error ETHGasTokenNotAllowed();
+    error ETHGasTokenRequired();
 
     /**
      * @notice Constructs new Adapter.
@@ -161,7 +140,6 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
         BridgeHubInterface _bridgeHub,
         WETH9Interface _l1Weth,
         address _l2RefundAddress,
-        FunderInterface _customGasTokenFunder,
         uint256 _l2GasLimit,
         uint256 _l1GasToL2GasPerPubDataLimit
     ) {
@@ -169,28 +147,26 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
         BRIDGE_HUB = _bridgeHub;
         L1_WETH = _l1Weth;
         L2_REFUND_ADDRESS = _l2RefundAddress;
-        CUSTOM_GAS_TOKEN_FUNDER = _customGasTokenFunder;
         L2_GAS_LIMIT = _l2GasLimit;
         L1_GAS_TO_L2_GAS_PER_PUB_DATA_LIMIT = _l1GasToL2GasPerPubDataLimit;
         SHARED_BRIDGE = BRIDGE_HUB.sharedBridge();
-        CUSTOM_GAS_TOKEN = BRIDGE_HUB.baseToken(CHAIN_ID);
-        if (CUSTOM_GAS_TOKEN == address(1)) {
-            revert ETHGasTokenNotAllowed();
+        address gasToken = BRIDGE_HUB.baseToken(CHAIN_ID);
+        if (gasToken != address(1)) {
+            revert ETHGasTokenRequired();
         }
     }
 
     /**
      * @notice Send cross-chain message to target on ZkStack.
-     * @dev The CUSTOM_GAS_TOKEN_FUNDER must hold enough of the gas token to pay for the L2 txn.
+     * @dev The HubPool must hold enough ETH to pay for the L2 txn.
      * @param target Contract on L2 that will receive message.
      * @param message Data to send to target.
      */
     function relayMessage(address target, bytes memory message) external payable override {
-        uint256 txBaseCost = _pullCustomGas(L2_GAS_LIMIT);
-        IERC20(CUSTOM_GAS_TOKEN).safeIncreaseAllowance(SHARED_BRIDGE, txBaseCost);
+        uint256 txBaseCost = _computeETHTxCost(L2_GAS_LIMIT);
 
         // Returns the hash of the requested L2 transaction. This hash can be used to follow the transaction status.
-        bytes32 canonicalTxHash = BRIDGE_HUB.requestL2TransactionDirect(
+        bytes32 canonicalTxHash = BRIDGE_HUB.requestL2TransactionDirect{ value: txBaseCost }(
             BridgeHubInterface.L2TransactionRequestDirect({
                 chainId: CHAIN_ID,
                 mintValue: txBaseCost,
@@ -210,7 +186,7 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
 
     /**
      * @notice Bridge tokens to ZkStack.
-     * @dev The CUSTOM_GAS_TOKEN_FUNDER must hold enough of the gas token to pay for the L2 txn.
+     * @dev The HubPool must hold enough ETH to pay for the L2 txn.
      * @param l1Token L1 token to deposit.
      * @param l2Token L2 token to receive.
      * @param amount Amount of L1 tokens to deposit and L2 tokens to receive.
@@ -224,31 +200,14 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
     ) external payable override {
         // A bypass proxy seems to no longer be needed to avoid deposit limits. The tracking of these limits seems to be deprecated.
         // See: https://github.com/matter-labs/era-contracts/blob/bce4b2d0f34bd87f1aaadd291772935afb1c3bd6/l1-contracts/contracts/bridge/L1ERC20Bridge.sol#L54-L55
-        uint256 txBaseCost = _pullCustomGas(L2_GAS_LIMIT);
+        uint256 txBaseCost = _computeETHTxCost(L2_GAS_LIMIT);
 
         bytes32 txHash;
         if (l1Token == address(L1_WETH)) {
             // If the l1Token is WETH then unwrap it to ETH then send the ETH to the standard bridge along with the base
-            // cost of custom gas tokens.
+            // cost.
             L1_WETH.withdraw(amount);
-            IERC20(CUSTOM_GAS_TOKEN).safeIncreaseAllowance(SHARED_BRIDGE, txBaseCost);
-            txHash = BRIDGE_HUB.requestL2TransactionTwoBridges{ value: amount }(
-                BridgeHubInterface.L2TransactionRequestTwoBridgesOuter({
-                    chainId: CHAIN_ID,
-                    mintValue: txBaseCost,
-                    l2Value: 0,
-                    l2GasLimit: L2_GAS_LIMIT,
-                    l2GasPerPubdataByteLimit: L1_GAS_TO_L2_GAS_PER_PUB_DATA_LIMIT,
-                    refundRecipient: L2_REFUND_ADDRESS,
-                    secondBridgeAddress: BRIDGE_HUB.sharedBridge(),
-                    secondBridgeValue: amount,
-                    secondBridgeCalldata: _secondBridgeCalldata(to, address(1), amount)
-                })
-            );
-        } else if (l1Token == CUSTOM_GAS_TOKEN) {
-            // The chain's custom gas token.
-            IERC20(l1Token).safeIncreaseAllowance(SHARED_BRIDGE, txBaseCost + amount);
-            txHash = BRIDGE_HUB.requestL2TransactionDirect(
+            txHash = BRIDGE_HUB.requestL2TransactionDirect{ value: amount + txBaseCost }(
                 BridgeHubInterface.L2TransactionRequestDirect({
                     chainId: CHAIN_ID,
                     mintValue: txBaseCost,
@@ -262,10 +221,9 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
                 })
             );
         } else {
-            // An ERC20 that is not WETH and not the custom gas token.
-            IERC20(CUSTOM_GAS_TOKEN).safeIncreaseAllowance(SHARED_BRIDGE, txBaseCost);
+            // An ERC20 that is not WETH.
             IERC20(l1Token).safeIncreaseAllowance(SHARED_BRIDGE, amount);
-            txHash = BRIDGE_HUB.requestL2TransactionTwoBridges(
+            txHash = BRIDGE_HUB.requestL2TransactionTwoBridges{ value: txBaseCost }(
                 BridgeHubInterface.L2TransactionRequestTwoBridgesOuter({
                     chainId: CHAIN_ID,
                     mintValue: txBaseCost,
@@ -300,21 +258,12 @@ contract ZkStack_CustomGasToken_Adapter is AdapterInterface {
     }
 
     /**
-     * @notice For a given l2 gas limit, this computes the amount of tokens needed, pulls them from the funder, and
+     * @notice For a given l2 gas limit, this computes the amount of ETH needed and
      * returns the amount.
-     * @dev Should return a value in the same precision as the gas token's precision.
      * @param l2GasLimit L2 gas limit for the message.
-     * @return amount of gas token that this contract needs to provide in order for the l2 transaction to succeed.
+     * @return amount of ETH that this contract needs to provide in order for the l2 transaction to succeed.
      */
-    function _pullCustomGas(uint256 l2GasLimit) internal returns (uint256) {
-        uint256 cost = BRIDGE_HUB.l2TransactionBaseCost(
-            CHAIN_ID,
-            tx.gasprice,
-            l2GasLimit,
-            L1_GAS_TO_L2_GAS_PER_PUB_DATA_LIMIT
-        );
-        CUSTOM_GAS_TOKEN_FUNDER.withdraw(IERC20(CUSTOM_GAS_TOKEN), cost);
-
-        return cost;
+    function _computeETHTxCost(uint256 l2GasLimit) internal view returns (uint256) {
+        return BRIDGE_HUB.l2TransactionBaseCost(CHAIN_ID, tx.gasprice, l2GasLimit, L1_GAS_TO_L2_GAS_PER_PUB_DATA_LIMIT);
     }
 }
