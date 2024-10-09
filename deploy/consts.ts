84e00d1c946f9754a5086142ed6abd0c784cb269
Paul <108695806+pxrl@users.noreply.github.com>
2024-10-09 13:54:00 +0000

    fix: Support OP bridged USDC in WorldChain adapter

--- deploy/consts.ts
+++ deploy/consts.ts
@@ -48,6 +48,7 @@ export const L1_ADDRESS_MAP: { [key: number]: { [contractName: string]: string }
     zoraStandardBridge: "0x3e2Ea9B92B7E48A52296fD261dc26fd995284631",
     worldChainCrossDomainMessenger: "0xf931a81D18B1766d15695ffc7c1920a62b7e710a",
     worldChainStandardBridge: "0x470458C91978D2d929704489Ad730DC3E3001113",
+    worldChainOpUSDCBridge: "0x153A69e4bb6fEDBbAaF463CB982416316c84B2dB",
   },
   [CHAIN_IDs.SEPOLIA]: {
     optimismCrossDomainMessenger: "0x58Cc85b8D04EA49cC6DBd3CbFFd00B4B8D6cb3ef",
