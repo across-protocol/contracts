d52f40f0c1df5fa0ebd3a676a9e8bcc8b2046537
Paul <108695806+pxrl@users.noreply.github.com>
2024-10-16 13:14:16 +0000

    Use multicall

--- tasks/verifySpokePool.ts
+++ tasks/verifySpokePool.ts
@@ -16,6 +16,12 @@ task("verify-spokepool", "Verify the configuration of a deployed SpokePool")
   .addFlag("routes", "Dump deposit route history (if any)")
   .setAction(async function (args, hre: HardhatRuntimeEnvironment) {
     const { deployments, ethers, companionNetworks, getChainId, network } = hre;
+
+    const bytes32ToAddress = (bytestring: string) => {
+      assert(bytestring.slice(2, 26) === "000000000000000000000000");
+      return ethers.utils.getAddress(bytestring.slice(0, 2).concat(bytestring.slice(-40)));
+    };
+
     const spokeChainId = parseInt(await getChainId());
     const hubChainId = Object.values(MAINNET_CHAIN_IDs).includes(spokeChainId) ? MAINNET : SEPOLIA;
 
@@ -28,34 +34,48 @@ task("verify-spokepool", "Verify the configuration of a deployed SpokePool")
     const { abi } = await deployments.get("Ethereum_SpokePool");
     const spokePool = new ethers.Contract(spokeAddress, abi, provider);
 
+    const spokeFunctions = [
+      "chainId",
+      "getCurrentTime",
+      "depositQuoteTimeBuffer",
+      "fillDeadlineBuffer",
+      "wrappedNativeToken",
+      // "withdrawalRecipient",
+      "crossDomainAdmin",
+    ] as const;
+    const multicall = await spokePool.callStatic.multicall(
+      spokeFunctions.map((fn) => spokePool.interface.encodeFunctionData(fn))
+    );
+    const results = Object.fromEntries(spokeFunctions.map((fn, idx) => [fn, multicall[idx]]));
+
     // Log state from SpokePool
-    const originChainId = await spokePool.chainId();
+    const originChainId = Number(results.chainId);
     console.log("SpokePool.chainId()".padEnd(TEXT_PADDING) + ": " + originChainId);
     assert(Number(originChainId) === spokeChainId, `${originChainId} != ${spokeChainId}`);
 
-    const currentTime = await spokePool.getCurrentTime();
+    const currentTime = Number(results.getCurrentTime);
     const formattedTime = `${currentTime} (${new Date(Number(currentTime) * 1000).toUTCString()})`;
     console.log("SpokePool.getCurrentTime()".padEnd(TEXT_PADDING) + ": " + formattedTime);
 
-    const quoteTimeBuffer = await spokePool.depositQuoteTimeBuffer();
+    const quoteTimeBuffer = Number(results.depositQuoteTimeBuffer);
     console.log("SpokePool.depositQuoteTimeBuffer()".padEnd(TEXT_PADDING) + ": " + quoteTimeBuffer);
 
-    const fillDeadlineBuffer = await spokePool.fillDeadlineBuffer();
+    const fillDeadlineBuffer = Number(results.fillDeadlineBuffer);
     console.log("SpokePool.fillDeadlineBuffer()".padEnd(TEXT_PADDING) + ": " + fillDeadlineBuffer);
 
-    const wrappedNative = await spokePool.wrappedNativeToken();
+    const wrappedNative = bytes32ToAddress(results.wrappedNativeToken);
     const wrappedNativeSymbol = spokeChainId === POLYGON ? "WMATIC" : "WETH";
     const expectedWrappedNative = TOKEN_SYMBOLS_MAP[wrappedNativeSymbol].addresses[spokeChainId];
     assert(wrappedNative === expectedWrappedNative, `wrappedNativeToken: ${wrappedNative} != ${expectedWrappedNative}`);
     console.log("SpokePool.wrappedNativeToken()".padEnd(TEXT_PADDING) + ": " + wrappedNative);
 
-    const withdrawalRecipient = await spokePool.withdrawalRecipient();
-    console.log("SpokePool.withdrawalRecipient()".padEnd(TEXT_PADDING) + ": " + withdrawalRecipient);
-    assert(withdrawalRecipient === hubAddress, `withdrawalRecipient: ${withdrawalRecipient} != ${hubAddress}`);
+    //    const withdrawalRecipient = bytes32ToAddress(results.withdrawalRecipient);
+    //    console.log("SpokePool.withdrawalRecipient()".padEnd(TEXT_PADDING) + ": " + withdrawalRecipient);
+    //    assert(withdrawalRecipient === hubAddress, `withdrawalRecipient: ${withdrawalRecipient} != ${hubAddress}`);
 
-    const admin = await spokePool.crossDomainAdmin();
-    console.log("SpokePool.crossDomainAdmin()".padEnd(TEXT_PADDING) + ": " + admin);
-    assert(admin === hubAddress, `${admin} != ${hubAddress}`);
+    const crossDomainAdmin = bytes32ToAddress(results.crossDomainAdmin);
+    console.log("SpokePool.crossDomainAdmin()".padEnd(TEXT_PADDING) + ": " + crossDomainAdmin);
+    assert(crossDomainAdmin === hubAddress, `${crossDomainAdmin} != ${hubAddress}`);
 
     // Log EnabledDepositRoute on SpokePool to test that L1 message arrived to L2:
     const filter = spokePool.filters.EnabledDepositRoute();
