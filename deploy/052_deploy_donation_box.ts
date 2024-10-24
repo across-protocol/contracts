8e2018374154198f9fdcc56bef5416af7eaf6992
bennett <bennett@umaproject.org>
2024-10-24 15:25:47 -0500

    remove comment
    Signed-off-by: bennett <bennett@umaproject.org>

--- deploy/052_deploy_donation_box.ts
+++ deploy/052_deploy_donation_box.ts
@@ -4,9 +4,6 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
 
-  // @note if deploying this contract on a chain like Linea that only supports up to
-  // solc 0.8.19, the hardhat.config solc version needs to be overridden and this
-  // contract needs to be recompiled.
   await hre.deployments.deploy("DonationBox", {
     contract: "DonationBox",
     from: deployer,
