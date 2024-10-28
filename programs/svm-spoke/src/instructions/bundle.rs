0e09d554da60dfab86fb0f5f22783c82b2e30560
Chris Maree <christopher.maree@gmail.com>
2024-10-28 11:59:24 +0100

    fix: better seed usage and improve signer macro location (#691)

--- programs/svm-spoke/src/instructions/bundle.rs
+++ programs/svm-spoke/src/instructions/bundle.rs
@@ -14,6 +14,9 @@ use crate::{
 #[event_cpi]
 #[derive(Accounts)]
 pub struct ExecuteRelayerRefundLeaf<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
     #[account(
         seeds = [b"instruction_params", signer.key().as_ref()],
         bump
@@ -34,9 +37,6 @@ pub struct ExecuteRelayerRefundLeaf<'info> {
         realloc::zero = false
     )]
     pub root_bundle: Account<'info, RootBundle>,
-
-    #[account(mut)]
-    pub signer: Signer<'info>, // TODO: put signers first everywhere.
 
     #[account(
         mut,
