58990c67ddd5839750d2b4b55fd684923b40db69
james-a-morris <jaamorris@cs.stonybrook.edu>
2024-10-29 10:58:25 -0400

    feat(alephzero): deploy contracts
    Signed-off-by: james-a-morris <jaamorris@cs.stonybrook.edu>

---
+++ deploy/054_deploy_alephzero_spokepool.ts
@@ -0,0 +1,32 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { deployNewProxy, getSpokePoolDeploymentInfo } from "../utils/utils.hre";
+import { FILL_DEADLINE_BUFFER, L2_ADDRESS_MAP, QUOTE_TIME_BUFFER, WAZERO, ZERO_ADDRESS } from "./consts";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { hubPool, spokeChainId } = await getSpokePoolDeploymentInfo(hre);
+
+  const initArgs = [
+    // Initialize deposit counter to very high number of deposits to avoid duplicate deposit ID's
+    // with deprecated spoke pool.
+    0,
+    L2_ADDRESS_MAP[spokeChainId].l2GatewayRouter,
+    // Set hub pool as cross domain admin since it delegatecalls the Adapter logic.
+    hubPool.address,
+    hubPool.address,
+  ];
+
+  const constructorArgs = [
+    WAZERO[spokeChainId],
+    QUOTE_TIME_BUFFER,
+    FILL_DEADLINE_BUFFER,
+    ZERO_ADDRESS,
+    // L2_ADDRESS_MAP[spokeChainId].cctpTokenMessenger,
+    // For now, we are not using the CCTP bridge and can disable by setting
+    // the cctpTokenMessenger to the zero address.
+    ZERO_ADDRESS,
+  ];
+  await deployNewProxy("AlephZero_SpokePool", constructorArgs, initArgs);
+};
+module.exports = func;
+func.tags = ["AlephZeroSpokePool", "alephzero"];
