825cad6e82de764fb1f405b2ed3f5e418b930c2b
chrismaree <christopher.maree@gmail.com>
2024-11-05 13:50:54 +0100

    WIP
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- programs/svm-spoke/src/lib.rs
+++ programs/svm-spoke/src/lib.rs
@@ -172,7 +172,7 @@ pub mod svm_spoke {
         instructions::fill_v3_relay(ctx, relay_data, repayment_chain_id, repayment_address)
     }
 
-    pub fn close_fill_pda(ctx: Context<CloseFillPda>, relay_data: V3RelayData) -> Result<()> {
+    pub fn close_fill_pda(ctx: Context<CloseFillPda>, _relay_hash: [u8; 32], relay_data: V3RelayData) -> Result<()> {
         instructions::close_fill_pda(ctx, relay_data)
     }
 
