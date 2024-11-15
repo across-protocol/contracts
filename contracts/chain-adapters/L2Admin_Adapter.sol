8211a5ea3c7140ebec30e45c7a586c53ffb21766
bennett <bennett@umaproject.org>
2024-11-15 13:21:47 -0600

    consolidate adapters
    Signed-off-by: bennett <bennett@umaproject.org>

--- contracts/chain-adapters/L2Admin_Adapter.sol
+++ contracts/chain-adapters/L2Admin_Adapter.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 import { AdapterInterface } from "./interfaces/AdapterInterface.sol";
 
 /**
- * @notice Contract containing logic to send messages from L1 to a withdrawal helper contract on an L2. This adapter is used to
- * communicate directly with a withdrawal helper. While any message can be sent to the withdrawal helper with this contract,
- * it should generally be used to perform upgrades to the withdrawal helper proxy.
+ * @notice Contract containing logic to send messages from L1 to a withdrawal helper or forwarder contract on an L2. This adapter is used to
+ * communicate directly with those contracts. While any message can be sent to those contracts with this adapter,
+ * it should generally be used to perform upgrades to their proxies.
  * @dev Public functions calling external contracts do not guard against reentrancy because they are expected to be
  * called via delegatecall, which will execute this contract's logic within the context of the originating contract.
  * For example, the HubPool will delegatecall these functions, therefore its only necessary that the HubPool's methods
@@ -15,29 +15,26 @@ import { AdapterInterface } from "./interfaces/AdapterInterface.sol";
  */
 
 // solhint-disable-next-line contract-name-camelcase
-contract WithdrawalHelperAdmin_Adapter is AdapterInterface {
+contract L2Admin_Adapter is AdapterInterface {
     // Adapter designed to relay messages from L1 to L2 addresses and delegatecalled by this contract to send messages to the forwarder
     // contract on L2.
     address public immutable L1_ADAPTER;
-    // Address of the withdrawal helper contract on L2. This is the value which overwrites the hub pool's supplied value in `relayMessage`.
-    address public immutable WITHDRAWAL_HELPER;
 
     error RelayMessageFailed();
 
     /**
-     * @notice Constructs new Adapter. This contract will use the L1_ADAPTER contract to send admin messages to `WITHDRAWAL_HELPER` on L2.
+     * @notice Constructs new Adapter. This contract will use the L1_ADAPTER contract to send admin messages to a target on L2.
      * @param _l1Adapter Address of the adapter contract on mainnet which implements message transfers and token relays to the L2
-     * where _forwarder is deployed.
-     * @param _withdrawalHelper Address of the withdrawal helper contract on L2 which receives the admin messages.
+     * where the L2 target is deployed.
      */
-    constructor(address _l1Adapter, address _withdrawalHelper) {
+    constructor(address _l1Adapter) {
         L1_ADAPTER = _l1Adapter;
-        WITHDRAWAL_HELPER = _withdrawalHelper;
     }
 
     function relayMessage(address, bytes memory message) external payable override {
+        (address target, bytes memory _relayMessage) = abi.decode(message, (address, bytes));
         (bool success, ) = L1_ADAPTER.delegatecall(
-            abi.encodeCall(AdapterInterface.relayMessage, (WITHDRAWAL_HELPER, message))
+            abi.encodeCall(AdapterInterface.relayMessage, (target, _relayMessage))
         );
         if (!success) revert RelayMessageFailed();
     }
@@ -48,6 +45,6 @@ contract WithdrawalHelperAdmin_Adapter is AdapterInterface {
         uint256,
         address
     ) external payable override {
-        revert("Cannot relay tokens to a withdrawal helper");
+        revert("Cannot relay tokens with the L2Admin_Adapter");
     }
 }
