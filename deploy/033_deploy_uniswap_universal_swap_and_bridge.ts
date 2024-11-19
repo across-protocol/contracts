c293c26ba8448aeabf13d0df37aad991d2f6ae5d
bennett <bennett@umaproject.org>
2024-11-19 13:43:25 -0600

    update deploy script
    Signed-off-by: bennett <bennett@umaproject.org>

--- deploy/033_deploy_uniswap_universal_swap_and_bridge.ts
+++ deploy/033_deploy_uniswap_universal_swap_and_bridge.ts
@@ -1,19 +1,24 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { getDeployedAddress } from "../src/DeploymentUtils";
-import { L1_ADDRESS_MAP, L2_ADDRESS_MAP } from "./consts";
+import { CHAIN_IDs } from "../utils";
+import { L1_ADDRESS_MAP, L2_ADDRESS_MAP, WETH, WMATIC } from "./consts";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const chainId = parseInt(await hre.getChainId());
+  const wrappedNativeToken =
+    chainId === CHAIN_IDs.POLYGON || chainId === CHAIN_IDs.POLYGON_AMOY ? WMATIC[chainId] : WETH[chainId];
 
   await hre.deployments.deploy("UniswapV3_UniversalSwapAndBridge", {
     contract: "UniversalSwapAndBridge",
     from: deployer,
     log: true,
     skipIfAlreadyDeployed: false,
+    deterministicDeployment: "0x123456789abc", // Salt for the create2 call.
     args: [
       getDeployedAddress("SpokePool", chainId),
+      wrappedNativeToken,
       chainId === 1 ? L1_ADDRESS_MAP[chainId].uniswapV3SwapRouter02 : L2_ADDRESS_MAP[chainId].uniswapV3SwapRouter02,
       // Allows function selectors in Uniswap V3 SwapRouter02:
       // - exactInputSingle
