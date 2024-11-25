1a49f582283c5609890642694ee40ba1a6e4fb0c
chrismaree <christopher.maree@gmail.com>
2024-11-25 15:35:23 +0700

    WIP
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- programs/svm-spoke/src/state/instruction_params.rs
+++ programs/svm-spoke/src/state/instruction_params.rs
@@ -5,8 +5,8 @@ use crate::RelayerRefundLeaf;
 #[account]
 #[derive(InitSpace)]
 pub struct ExecuteRelayerRefundLeafParams {
-    pub root_bundle_id: u32,
-    pub relayer_refund_leaf: RelayerRefundLeaf,
+    pub root_bundle_id: u32,                    // ID of the root bundle to be used.
+    pub relayer_refund_leaf: RelayerRefundLeaf, // Leaf to be verified against the proof and instruct bundle execution.
     #[max_len(0)]
-    pub proof: Vec<[u8; 32]>,
+    pub proof: Vec<[u8; 32]>,  // Proof to verify the leaf's inclusion in relayer refund merkle tree.
 }
