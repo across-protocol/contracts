8ca7cd7aeaca571bc06fc303d08970d8c6beb7ab
Chris Maree <christopher.maree@gmail.com>
2024-11-27 15:24:46 +0700

    Discard changes to programs/svm-spoke/src/state/instruction_params.rs

--- programs/svm-spoke/src/state/instruction_params.rs
+++ programs/svm-spoke/src/state/instruction_params.rs
@@ -8,7 +8,7 @@ pub struct ExecuteRelayerRefundLeafParams {
     pub root_bundle_id: u32,                    // ID of the root bundle to be used.
     pub relayer_refund_leaf: RelayerRefundLeaf, // Leaf to be verified against the proof and instruct bundle execution.
     #[max_len(0)]
-    pub proof: Vec<[u8; 32]>, // Proof to verify the leaf's inclusion in relayer refund merkle tree.
+    pub proof: Vec<[u8; 32]>,  // Proof to verify the leaf's inclusion in relayer refund merkle tree.
 }
 
 #[account]
