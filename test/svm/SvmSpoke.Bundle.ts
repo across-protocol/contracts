8e8370f89d8ec1a4e987e3c5b71a134e5b4d2b80
chrismaree <christopher.maree@gmail.com>
2024-12-03 10:42:14 +0100

    WIP
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- test/svm/SvmSpoke.Bundle.ts
+++ test/svm/SvmSpoke.Bundle.ts
@@ -279,8 +279,8 @@ describe("svm_spoke.bundle", () => {
     assert.isFalse(event.deferredRefunds, "deferredRefunds should be false");
     assertSE(event.caller, owner, "caller should match");
 
-    event = events.find((event) => event.name === "tokensBridged").data;
-
+    // Verify the tokensBridged event
+    event = events.find((event) => event.name === "tokensBridged")?.data;
     assertSE(event.amountToReturn, relayerRefundLeaves[0].amountToReturn, "amountToReturn should match");
     assertSE(event.chainId, chainId, "chainId should match");
     assertSE(event.leafId, leaf.leafId, "leafId should match");
