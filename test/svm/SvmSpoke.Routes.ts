a5fb1de3d53832b892c784ce559081131ef93708
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2024-12-06 12:09:48 +0000

    fix: test
    Signed-off-by: Pablo Maldonado <pablomaldonadoturci@gmail.com>

--- test/svm/SvmSpoke.Routes.ts
+++ test/svm/SvmSpoke.Routes.ts
@@ -4,7 +4,7 @@ import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, createMint, getAccount }
 import { PublicKey, Keypair } from "@solana/web3.js";
 import { assert } from "chai";
 import { common } from "./SvmSpoke.common";
-import { readEventsUntilFound } from "./utils";
+import { readEventsUntilFound } from "../../src/svm";
 
 const { provider, program, owner, initializeState, createRoutePda, getVaultAta } = common;
 
