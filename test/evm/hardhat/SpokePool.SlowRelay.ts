326600c2220f72c71026a3f872d225479aa4c742
Chris Maree <christopher.maree@gmail.com>
2024-12-18 14:31:21 +0100

    WIP
    Signed-off-by: Chris Maree <christopher.maree@gmail.com>

--- test/evm/hardhat/SpokePool.SlowRelay.ts
+++ test/evm/hardhat/SpokePool.SlowRelay.ts
@@ -12,7 +12,7 @@ import {
 import { spokePoolFixture, V3RelayData, getV3RelayHash, V3SlowFill, FillType } from "./fixtures/SpokePool.Fixture";
 import { buildV3SlowRelayTree } from "./MerkleLib.utils";
 import * as consts from "./constants";
-import { FillStatus } from "../../../utils/constants";
+import { FillStatus, fillV3RelayBytes } from "../../../utils/constants";
 
 let spokePool: Contract, erc20: Contract, destErc20: Contract;
 let depositor: SignerWithAddress, recipient: SignerWithAddress, relayer: SignerWithAddress;
@@ -91,7 +91,9 @@ describe("SpokePool Slow Relay Logic", async function () {
       );
 
       // Can fast fill after:
-      await spokePool.connect(relayer).fillV3Relay(relayData, consts.repaymentChainId, addressToBytes(relayer.address));
+      await spokePool
+        .connect(relayer)
+        [fillV3RelayBytes](relayData, consts.repaymentChainId, addressToBytes(relayer.address));
     });
     it("cannot request if FillStatus is Filled", async function () {
       const relayHash = getV3RelayHash(relayData, consts.destinationChainId);
@@ -175,7 +177,7 @@ describe("SpokePool Slow Relay Logic", async function () {
       await expect(
         spokePool
           .connect(relayer)
-          .fillV3Relay(slowRelayLeaf.relayData, consts.repaymentChainId, addressToBytes(relayer.address))
+          [fillV3RelayBytes](slowRelayLeaf.relayData, consts.repaymentChainId, addressToBytes(relayer.address))
       ).to.be.revertedWith("RelayFilled");
     });
     it("cannot be used to double send a fill", async function () {
@@ -185,7 +187,7 @@ describe("SpokePool Slow Relay Logic", async function () {
       // Fill before executing slow fill
       await spokePool
         .connect(relayer)
-        .fillV3Relay(slowRelayLeaf.relayData, consts.repaymentChainId, addressToBytes(relayer.address));
+        [fillV3RelayBytes](slowRelayLeaf.relayData, consts.repaymentChainId, addressToBytes(relayer.address));
       await expect(
         spokePool.connect(relayer).executeV3SlowRelayLeaf(
           slowRelayLeaf,
