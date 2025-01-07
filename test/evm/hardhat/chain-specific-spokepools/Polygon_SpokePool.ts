7972b058505b8c5530d468f89f90fd916a3dc186
Chris Maree <christopher.maree@gmail.com>
2025-01-07 18:29:56 +0100

    Discard changes to test/evm/hardhat/chain-specific-spokepools/Polygon_SpokePool.ts

--- test/evm/hardhat/chain-specific-spokepools/Polygon_SpokePool.ts
+++ test/evm/hardhat/chain-specific-spokepools/Polygon_SpokePool.ts
@@ -336,14 +336,16 @@ describe("Polygon Spoke Pool", function () {
       message: "0x1234",
     };
     const fillData = [
-      polygonSpokePool.interface.encodeFunctionData(
-        "fillV3RelayBytes32((bytes32,bytes32,bytes32,bytes32,bytes32,uint256,uint256,uint256,uint256,uint32,uint32,bytes),uint256,bytes32)",
-        [relayData, repaymentChainId, addressToBytes(relayer.address)]
-      ),
-      polygonSpokePool.interface.encodeFunctionData(
-        "fillV3RelayBytes32((bytes32,bytes32,bytes32,bytes32,bytes32,uint256,uint256,uint256,uint256,uint32,uint32,bytes),uint256,bytes32)",
-        [{ ...relayData, depositId: 1 }, repaymentChainId, addressToBytes(relayer.address)]
-      ),
+      polygonSpokePool.interface.encodeFunctionData("fillV3Relay", [
+        relayData,
+        repaymentChainId,
+        addressToBytes(relayer.address),
+      ]),
+      polygonSpokePool.interface.encodeFunctionData("fillV3Relay", [
+        { ...relayData, depositId: 1 },
+        repaymentChainId,
+        addressToBytes(relayer.address),
+      ]),
     ];
     const otherData = [polygonSpokePool.interface.encodeFunctionData("wrap", [])];
 
