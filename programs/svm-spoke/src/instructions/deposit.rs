e2d188224ee01ae1ebe7455697464d8839fb237e
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2025-01-08 14:38:43 +0000

    feat(svm): L-02 Missing SpeedUpV3Deposit and FillV3RelayWithUpdatedDeposit Functionalities in SVM Spoke Pool
    Signed-off-by: Pablo Maldonado <pablomaldonadoturci@gmail.com>

--- programs/svm-spoke/src/instructions/deposit.rs
+++ programs/svm-spoke/src/instructions/deposit.rs
@@ -1,3 +1,9 @@
+// Note: The `svm-spoke` does not support `speedUpV3Deposit` and `fillV3RelayWithUpdatedDeposit` due to cryptographic
+// incompatibilities between Solana (Ed25519) and Ethereum (ECDSA secp256k1). Specifically, Solana wallets cannot
+// generate ECDSA signatures required for Ethereum verification.
+// As a result, speed-up functionality on Solana is not implemented.
+// For more details, refer to the documentation: https://docs.across.to/.
+
 use anchor_lang::prelude::*;
 use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
