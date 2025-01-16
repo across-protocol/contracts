a105f914c6195f4122d44965d1f3bb5a96642722
Pablo Maldonado <pablo@umaproject.org>
2025-01-16 15:36:41 +0000

    feat(svm): web3 v2 events
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- src/svm/v2/solanaProgramUtilsV2.ts
+++ src/svm/v2/solanaProgramUtilsV2.ts
@@ -12,29 +12,19 @@ import web3, {
 type GetTransactionReturnType = ReturnType<GetTransactionApi["getTransaction"]>;
 
 type GetSignaturesForAddressConfig = Readonly<{
-  /** start searching backwards from this transaction signature. If not provided the search starts from the top of the highest max confirmed block. */
   before?: Signature;
   commitment?: Exclude<Commitment, "processed">;
-  /** maximum transaction signatures to return (between 1 and 1,000). Default: 1000 */
   limit?: number;
-  /** The minimum slot that the request can be evaluated at */
   minContextSlot?: Slot;
-  /** search until this transaction signature, if found before limit reached */
   until?: Signature;
 }>;
 
 type GetSignaturesForAddressTransaction = {
-  /** estimated production time of when transaction was processed. null if not available. */
   blockTime: UnixTimestamp | null;
-  /** The transaction's cluster confirmation status */
   confirmationStatus: Commitment | null;
-  /** Error if transaction failed, null if transaction succeeded. */
   err: TransactionError | null;
-  /** Memo associated with the transaction, null if no memo is present */
   memo: string | null;
-  /** transaction signature as base-58 encoded string */
   signature: Signature;
-  /** The slot that contains the block with the transaction */
   slot: Slot;
 };
 
