f4c76131a334bd00db25dc0943de858beb515d26
Paul <108695806+pxrl@users.noreply.github.com>
2025-01-23 16:31:30 +0000

    Revert "fix(SpokePool): Reconstitute FilledV3Relay events (#872)"
    This reverts commit ebae3a23c70300c09a9992f9bbd63e8473fdf88b.
    
    The fix was wrong, and it shouldn't have gone into this branch anyway.
    Instead of `address messageHash`, it should have specified `bytes
    message`.

--- contracts/interfaces/V3SpokePoolInterface.sol
+++ contracts/interfaces/V3SpokePoolInterface.sol
@@ -109,15 +109,6 @@ interface V3SpokePoolInterface {
         FillType fillType;
     }
 
-    // Same as V3RelayExecutionEventInfo but using addresses instead of bytes32. Currently retained for historical
-    // reference. Will be deprecated in favor of V3RelayExecutionEventInfo in the future.
-    struct V3RelayExecutionEventInfoLegacy {
-        address updatedRecipient;
-        address updatedMessageHash;
-        uint256 updatedOutputAmount;
-        FillType fillType;
-    }
-
     // Represents the parameters required for a V3 deposit operation in the SpokePool.
     struct DepositV3Params {
         bytes32 depositor;
@@ -397,7 +388,7 @@ interface V3SpokePoolInterface {
         address depositor,
         address recipient,
         bytes message,
-        V3RelayExecutionEventInfoLegacy relayExecutionInfo
+        V3RelayExecutionEventInfo relayExecutionInfo
     );
 
     event RequestedV3SlowFill(
