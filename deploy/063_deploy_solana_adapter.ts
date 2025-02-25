c2a233f34edea8854d3606a65259a10485ec464e
Pablo Maldonado <pablo@umaproject.org>
2025-02-25 18:17:04 +0100

    fix: fixes
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- deploy/063_deploy_solana_adapter.ts
+++ deploy/063_deploy_solana_adapter.ts
@@ -1,28 +1,13 @@
-import { utils } from "@coral-xyz/anchor";
+import { getAssociatedTokenAddressSync } from "@solana/spl-token";
+import { PublicKey } from "@solana/web3.js";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { getDeployedAddress } from "../src/DeploymentUtils";
+import { fromBase58ToBytes32 } from "../src/svm/web3-v1";
 import { SOLANA_SPOKE_STATE_SEED } from "../src/svm/web3-v1/constants";
 import { getSolanaChainId } from "../src/svm/web3-v1/helpers";
 import { CHAIN_IDs } from "../utils";
-import { USDC } from "./consts";
-import { getAssociatedTokenAddressSync } from "@solana/spl-token";
-import { PublicKey } from "@solana/web3.js";
-
-const fromBase58 = (input: string) => {
-  const decodedBytes = utils.bytes.bs58.decode(input);
-  return "0x" + Buffer.from(decodedBytes).toString("hex");
-};
-
-const CCTP_TOKEN_MESSENGER_V1 = {
-  [CHAIN_IDs.MAINNET]: "0xbd3fa81b58ba92a82136038b25adec7066af3155",
-  [CHAIN_IDs.SEPOLIA]: "0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5",
-};
-
-const CCTP_MESSAGE_TRANSMITTER_V1 = {
-  [CHAIN_IDs.MAINNET]: "0x0a992d191deec32afe36203ad87d7d289a738f81",
-  [CHAIN_IDs.SEPOLIA]: "0x7865fAfC2db2093669d92c0F33AeEF291086BEFD",
-};
+import { L1_ADDRESS_MAP, USDC } from "./consts";
 
 const SOLANA_USDC = {
   mainnet: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
@@ -35,19 +20,19 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const solanaTargetNetwork = chainId != CHAIN_IDs.MAINNET ? "devnet" : "mainnet";
 
   const l1Usdc = USDC[chainId];
-  const cctpTokenMessenger = CCTP_TOKEN_MESSENGER_V1[chainId];
-  const cctpMessageTransmitter = CCTP_MESSAGE_TRANSMITTER_V1[chainId];
+  const cctpTokenMessenger = L1_ADDRESS_MAP[chainId].cctpTokenMessenger;
+  const cctpMessageTransmitter = L1_ADDRESS_MAP[chainId].cctpMessageTransmitter;
   const solanaSpokePool = getDeployedAddress("SvmSpoke", getSolanaChainId(solanaTargetNetwork).toString());
   if (!solanaSpokePool) {
     throw new Error("SvmSpoke not deployed");
   }
-  const solanaUsdc = fromBase58(SOLANA_USDC[solanaTargetNetwork]);
+  const solanaUsdc = fromBase58ToBytes32(SOLANA_USDC[solanaTargetNetwork]);
   const mint = new PublicKey(SOLANA_USDC[solanaTargetNetwork]);
   const seeds = [Buffer.from("state"), SOLANA_SPOKE_STATE_SEED.toArrayLike(Buffer, "le", 8)];
   const [state] = PublicKey.findProgramAddressSync(seeds, new PublicKey(solanaSpokePool));
   const vault = getAssociatedTokenAddressSync(mint, state, true);
-  const solanaSpokePoolUsdcVault = fromBase58(vault.toBase58());
-  const solanaSpokePoolBytes32 = fromBase58(solanaSpokePool);
+  const solanaSpokePoolUsdcVault = fromBase58ToBytes32(vault.toBase58());
+  const solanaSpokePoolBytes32 = fromBase58ToBytes32(solanaSpokePool);
 
   await hre.deployments.deploy("Solana_Adapter", {
     from: deployer,
@@ -65,4 +50,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 
 module.exports = func;
-func.tags = ["Solana_Adapter"];
+func.tags = ["SolanaAdapter", "mainnet"];
