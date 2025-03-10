ad5500deb1c154fd32910460fb0d5c36e1ac05bb
Ihor Farion <ihor@umaproject.org>
2025-03-10 12:46:22 -0700

    update arbitrum spokepool gap
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- contracts/Arbitrum_SpokePool.sol
+++ contracts/Arbitrum_SpokePool.sol
@@ -175,5 +175,5 @@ contract Arbitrum_SpokePool is SpokePool, CircleCCTPAdapter, OFTTransportAdapter
     // Reserve storage slots for future versions of this base contract to add state variables without
     // affecting the storage layout of child contracts. Decrement the size of __gap whenever state variables
     // are added. This is at bottom of contract to make sure it's always at the end of storage.
-    uint256[1000] private __gap;
+    uint256[999] private __gap;
 }
