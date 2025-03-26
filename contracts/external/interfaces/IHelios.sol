ef947932e25e2f8c74a588d1ba3691e1d8061497
nicholaspai <npai.nyc@gmail.com>
2025-03-25 22:22:00 -0400

    Update IHelios.sol

--- contracts/external/interfaces/IHelios.sol
+++ contracts/external/interfaces/IHelios.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 /// https://github.com/succinctlabs/sp1-helios/blob/776337bf8b63bcf9beebad143e8981020dec2b52/contracts/src/SP1Helios.sol
 interface IHelios {
     /// @notice Gets the value of a storage slot at a specific block
+    /// @dev Function added to Helios in https://github.com/across-protocol/sp1-helios/pull/2
     function getStorageSlot(
         uint256 blockNumber,
         address contractAddress,
