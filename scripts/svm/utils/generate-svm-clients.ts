3742b0624c6aa76e533e41f53b235929c40480e0
Pablo Maldonado <pablo@umaproject.org>
2025-04-15 11:51:43 +0200

    feat(svm): update codama clients bigint depositid
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- scripts/svm/utils/generate-svm-clients.ts
+++ scripts/svm/utils/generate-svm-clients.ts
@@ -1,12 +1,29 @@
-import { createFromRoot } from "codama";
-import { rootNodeFromAnchor, AnchorIdl } from "@codama/nodes-from-anchor";
+import { AnchorIdl, rootNodeFromAnchor } from "@codama/nodes-from-anchor";
 import { renderVisitor as renderJavaScriptVisitor } from "@codama/renderers-js";
-import { SvmSpokeIdl, MulticallHandlerIdl } from "../../../src/svm/assets";
+import { bottomUpTransformerVisitor } from "@codama/visitors";
+import { createFromRoot, numberTypeNode } from "codama";
 import path from "path";
+import { MulticallHandlerIdl, SvmSpokeIdl } from "../../../src/svm/assets";
 export const clientsPath = path.join(__dirname, "..", "..", "..", "src", "svm", "clients");
+
+const transformDepositIdsVisitor = bottomUpTransformerVisitor([
+  {
+    select: (nodePath) => {
+      const lastNode = nodePath[nodePath.length - 1];
+      if (lastNode.kind !== "bytesTypeNode") return false;
+      function containsFieldName(node: any, targetName: string): boolean {
+        if (node?.name?.toLowerCase() === targetName.toLowerCase()) return true;
+        return false;
+      }
+      return nodePath.some((node) => containsFieldName(node, "depositId"));
+    },
+    transform: () => numberTypeNode("u64"),
+  },
+]);
 
 // Generate SvmSpoke clients
 let codama = createFromRoot(rootNodeFromAnchor(SvmSpokeIdl as AnchorIdl));
+codama.update(transformDepositIdsVisitor);
 codama.accept(renderJavaScriptVisitor(path.join(clientsPath, "SvmSpoke")));
 
 // Generate MulticallHandler clients
