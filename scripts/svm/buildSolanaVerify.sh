bbe3a57475e8d28fbf65bee1a4cbd2d93f2180f8
Reinis Martinsons <reinis@umaproject.org>
2025-04-29 14:30:58 +0000

    fix: target permissions
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- scripts/svm/buildSolanaVerify.sh
+++ scripts/svm/buildSolanaVerify.sh
@@ -7,6 +7,10 @@ else
   CARGO_OPTIONS=""
 fi
 
+# Determine current user and group
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+
 for program in programs/*; do
   [ -d "$program" ] || continue
 
@@ -16,8 +20,11 @@ for program in programs/*; do
   echo "Running verified build for $program_name"
   solana-verify build --library-name "$program_name" -- $CARGO_OPTIONS
 
+- #Fix target/ permissions after Docker build
+  sudo chown -R "$USER_ID:$GROUP_ID" target
+
   echo "Building IDL for $program_name"
-  ANCHOR_LOG=true anchor idl build -p "$dir_name" -o "target/idl/$program_name.json" -t "target/types/$program_name.ts" -- $CARGO_OPTIONS
+  anchor idl build -p "$dir_name" -o "target/idl/$program_name.json" -t "target/types/$program_name.ts" -- $CARGO_OPTIONS
 done
 
 echo "Generating external program types"
