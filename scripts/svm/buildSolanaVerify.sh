0a5f070066d00aad2aa140da6ca168f239cda8bf
Reinis Martinsons <reinis@umaproject.org>
2025-04-29 15:12:04 +0000

    fix: regular anchor build first
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- scripts/svm/buildSolanaVerify.sh
+++ scripts/svm/buildSolanaVerify.sh
@@ -7,26 +7,18 @@ else
   CARGO_OPTIONS=""
 fi
 
-# Determine current user and group
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
-
 for program in programs/*; do
   [ -d "$program" ] || continue
 
   dir_name=$(basename "$program")
   program_name=${dir_name//-/_}
 
+  # Regular build first so that it generates required IDL and types for the tests
+  echo "Building program $program_name"
+  anchor build -p "$dir_name" -- $CARGO_OPTIONS
+
   echo "Running verified build for $program_name"
   solana-verify build --library-name "$program_name" -- $CARGO_OPTIONS
-
-  echo "Fix target/ permissions after Docker build"
-  sudo chown -R "$USER_ID:$GROUP_ID" target
-
-  cargo test --no-run --locked --verbose
-
-  echo "Building IDL for $program_name"
-  anchor idl build -p "$dir_name" -o "target/idl/$program_name.json" -t "target/types/$program_name.ts" -- $CARGO_OPTIONS
 done
 
 echo "Generating external program types"
