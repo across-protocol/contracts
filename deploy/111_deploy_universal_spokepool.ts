f6b6c075a6b4a01c2c8a120bb18faec80e440de1
nicholaspai <npai.nyc@gmail.com>
2025-05-01 09:09:28 -0400

    Bump constants

--- deploy/111_deploy_universal_spokepool.ts
+++ deploy/111_deploy_universal_spokepool.ts
@@ -1,26 +1,28 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { deployNewProxy, getSpokePoolDeploymentInfo } from "../utils/utils.hre";
-import {
-  FILL_DEADLINE_BUFFER,
-  L1_ADDRESS_MAP,
-  L2_ADDRESS_MAP,
-  QUOTE_TIME_BUFFER,
-  WETH,
-  USDC,
-  ZERO_ADDRESS,
-} from "./consts";
-import { CHAIN_IDs } from "../utils/constants";
+import { FILL_DEADLINE_BUFFER, L1_ADDRESS_MAP, L2_ADDRESS_MAP, QUOTE_TIME_BUFFER, USDC, ZERO_ADDRESS } from "./consts";
+import { CHAIN_IDs, PRODUCTION_NETWORKS, TOKEN_SYMBOLS_MAP } from "../utils/constants";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { hubPool, spokeChainId } = await getSpokePoolDeploymentInfo(hre);
 
   const initArgs = [1, hubPool.address, hubPool.address];
+
+  // Get Wrapped native token address:
+  const nativeTokenSymbol = PRODUCTION_NETWORKS[spokeChainId].nativeToken;
+  const wrappedPrefix = "W";
+  const wrappedNativeSymbol = `${wrappedPrefix}${nativeTokenSymbol}`;
+  const expectedWrappedNative = TOKEN_SYMBOLS_MAP[wrappedNativeSymbol].addresses[spokeChainId];
+  if (!expectedWrappedNative) {
+    throw new Error(`Wrapped native token not found for ${wrappedNativeSymbol} on chainId ${spokeChainId}`);
+  }
+
   const constructorArgs = [
     24 * 60 * 60, // 1 day; Helios latest head timestamp must be 1 day old before an admin can force execute a message.
     L2_ADDRESS_MAP[spokeChainId]?.helios,
     L1_ADDRESS_MAP[CHAIN_IDs.MAINNET]?.hubPoolStore,
-    WETH[spokeChainId],
+    expectedWrappedNative,
     QUOTE_TIME_BUFFER,
     FILL_DEADLINE_BUFFER,
     USDC[spokeChainId] ?? ZERO_ADDRESS,
