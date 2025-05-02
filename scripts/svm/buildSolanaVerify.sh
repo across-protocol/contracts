9170f2711c7af0a1d5020b6b218ae18dfee28b98
Reinis Martinsons <reinis@umaproject.org>
2025-05-02 13:57:50 +0000

    fix: cleanup
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- scripts/svm/buildSolanaVerify.sh
+++ scripts/svm/buildSolanaVerify.sh
@@ -7,39 +7,27 @@ else
   CARGO_OPTIONS=""
 fi
 
+# Create required directories.
+mkdir -p target/idl
+mkdir -p target/types
+mkdir -p target/deploy
+
 for program in programs/*; do
   [ -d "$program" ] || continue
 
   dir_name=$(basename "$program")
   program_name=${dir_name//-/_}
 
-  # Regular build first so that it generates required IDL and types for the tests
-  # echo "Building program $program_name"
-  # anchor build -p "$dir_name" -- $CARGO_OPTIONS
+  # Verified build does not save IDL and types, so generate them here.
   echo "Generating IDL for $program_name"
-  mkdir -p target/idl
-  mkdir -p target/types
-  mkdir -p target/deploy
-  echo "target/idl/ before"
-  ls -la target/idl
-  echo "target/types/ before"
-  ls -la target/types
   anchor idl build \
     --program-name "$program_name" \
     --out "target/idl/$program_name.json" \
     --out-ts "target/types/$program_name.ts" \
     -- $CARGO_OPTIONS
-  echo "target/idl/ after"
-  ls -la target/idl
-  echo "target/types/ after"
-  ls -la target/types
 
   echo "Running verified build for $program_name"
-  echo "target/deploy/ before"
-  ls -la target/deploy
   solana-verify build --library-name "$program_name" -- $CARGO_OPTIONS
-  echo "target/deploy/ after"
-  ls -la target/deploy
 
   # We don't need keypair files from the verified build and they cause permission issues on CI
   echo "Removing target/deploy/$program_name-keypair.json"
