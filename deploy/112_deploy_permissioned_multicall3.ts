624454e1f6aa869871d8b197e2fae9a9e3a6ec9b
nicholaspai <npai.nyc@gmail.com>
2025-05-04 14:08:03 -0400

    Deploy

---
+++ deploy/112_deploy_permissioned_multicall3.ts
@@ -0,0 +1,16 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+
+  const instance = await hre.deployments.deploy("PermissionedMulticall3", {
+    from: deployer,
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+  await hre.run("verify:verify", { address: instance.address });
+};
+
+module.exports = func;
+func.tags = ["PermissionedMulticall3"];
