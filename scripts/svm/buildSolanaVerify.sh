19a5ca529434c0fc18fb91d8159142785594b398
Reinis Martinsons <reinis@umaproject.org>
2025-05-05 16:23:47 +0000

    fix: cleanup
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- scripts/svm/buildSolanaVerify.sh
+++ scripts/svm/buildSolanaVerify.sh
@@ -7,24 +7,11 @@ else
   CARGO_OPTIONS=""
 fi
 
-# Create required directories.
-mkdir -p target/idl
-mkdir -p target/types
-mkdir -p target/deploy
-
 for program in programs/*; do
   [ -d "$program" ] || continue
 
   dir_name=$(basename "$program")
   program_name=${dir_name//-/_}
-
-  # Verified build does not save IDL and types, so generate them here.
-  echo "Generating IDL for $program_name"
-  anchor idl build \
-    --program-name "$program_name" \
-    --out "target/idl/$program_name.json" \
-    --out-ts "target/types/$program_name.ts" \
-    -- $CARGO_OPTIONS
 
   echo "Running verified build for $program_name"
   solana-verify build --library-name "$program_name" -- $CARGO_OPTIONS
