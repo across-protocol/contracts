7d8aaa8ec2f4bbe5703f0588a3537d68446d037d
Reinis Martinsons <reinis@umaproject.org>
2025-05-12 15:08:57 +0000

    fix: skip removing keypairs for this experiment
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- scripts/svm/buildHelpers/buildSolanaVerify.sh
+++ scripts/svm/buildHelpers/buildSolanaVerify.sh
@@ -16,9 +16,4 @@ for program in programs/*; do
 
   echo "Running verified build for $program_name"
   solana-verify build --library-name "$program_name" -- $CARGO_OPTIONS
-
-  # We don't need keypair files from the verified build and they cause permission issues on CI
-  echo "Removing target/deploy/$program_name-keypair.json"
-  sudo rm -f "target/deploy/$program_name-keypair.json"
-
 done
