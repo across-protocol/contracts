27638345547c5ced52f71bbeb7c5793e30962d58
Matt Rice <matthewcrice32@gmail.com>
2025-06-23 22:27:05 -0400

    [M-05] Incorrect EIP-712 Encoding (#1017)
    * M-05 Incorrect EIP-712 Encoding
    
    Fix EIP-712 encoding issue by replacing TransferType enum with uint8
    in SwapAndDepositData type string and cast enum to uint8 in hash encoding.
    Add comments to TransferType enum values showing their integer mappings.
    
    * Remove unnecessary uint8 cast in EIP-712 encoding
    
    The cast to uint8 is redundant since the EIP-712 type string already
    specifies uint8 transferType, making the encoding identical without
    the explicit cast.

--- contracts/libraries/PeripherySigningLib.sol
+++ contracts/libraries/PeripherySigningLib.sol
@@ -10,7 +10,7 @@ library PeripherySigningLib {
     string internal constant EIP712_DEPOSIT_DATA_TYPE =
         "DepositData(Fees submissionFees,BaseDepositData baseDepositData,uint256 inputAmount,address spokePool,uint256 nonce)";
     string internal constant EIP712_SWAP_AND_DEPOSIT_DATA_TYPE =
-        "SwapAndDepositData(Fees submissionFees,BaseDepositData depositData,address swapToken,address exchange,TransferType transferType,uint256 swapTokenAmount,uint256 minExpectedInputTokenAmount,bytes routerCalldata,bool enableProportionalAdjustment,address spokePool,uint256 nonce)";
+        "SwapAndDepositData(Fees submissionFees,BaseDepositData depositData,address swapToken,address exchange,uint8 transferType,uint256 swapTokenAmount,uint256 minExpectedInputTokenAmount,bytes routerCalldata,bool enableProportionalAdjustment,address spokePool,uint256 nonce)";
 
     // EIP712 Type hashes.
     bytes32 internal constant EIP712_FEES_TYPEHASH = keccak256(abi.encodePacked(EIP712_FEES_TYPE));
