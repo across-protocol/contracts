358fd98f3cdcef17b19465dbaeaafa3e364a9217
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-07-21 14:10:42 +0200

    chore: new `MulticallHandler` deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -113,7 +113,7 @@ const config: HardhatUserConfig = {
         enabled: true,
       },
       suppressedErrors: ["sendtransfer"],
-      contractsToCompile: ["SpokePoolPeriphery"],
+      contractsToCompile: ["SpokePoolPeriphery", "MulticallHandler"],
     },
   },
   networks: {
