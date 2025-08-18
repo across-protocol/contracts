78e83e7e525099b8b78a765b92ff133dfaf71c8d
Ihor Farion <ihor@umaproject.org>
2025-08-18 15:23:34 -0700

    polish and fix bug
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- contracts/chain-adapters/AdminRelayTokensAdapter.sol
+++ contracts/chain-adapters/AdminRelayTokensAdapter.sol
@@ -7,25 +7,31 @@ import "./interfaces/AdapterInterface.sol";
  * @dev a custom adapter that allows admin to call `relayTokens` on adapters form HubPool by calling `relayMessage` on this adapter
  */
 contract AdminRelayTokensAdapter is AdapterInterface {
-    // @dev adapter to use to actually `relayTokens`
+    // @dev adapter to delegatecall `relayTokens` on
     address immutable targetAdapter;
+    bytes32 public constant ADMIN_SALT = keccak256("SEND_VIA_ADMIN_RELAY_TOKENS_ADAPTER");
 
     constructor(address _targetAdapter) {
         targetAdapter = _targetAdapter;
     }
 
+    /**
+     * @param target address of the spokepool which will be the receiver of tokens for the `relayTokens` call performed by this special adapter
+     * @param message abi-encoded arguments: (bytes32 adminSalt, address l1Token, address l2Token, uint256 amount, address spokePool). They include
+     * admin salt + params required to call a `relayTokens` function on an underlying `targetAdapter`
+     */
     function relayMessage(address target, bytes calldata message) external payable {
         (bytes32 adminSalt, address l1Token, address l2Token, uint256 amount, address spokePool) = abi.decode(
             message,
             (bytes32, address, address, uint256, address)
         );
-        require(adminSalt == requiredAdminSalt(), "incorrect admin salt");
+        require(adminSalt == ADMIN_SALT, "incorrect admin salt");
         require(target == spokePool, "target / spokepool mismatch");
 
         // We are ok with this low-level call since the adapter address is set by the admin and we've
         // already checked that its not the zero address.
         // solhint-disable-next-line avoid-low-level-calls
-        (bool success, ) = target.delegatecall(
+        (bool success, ) = targetAdapter.delegatecall(
             abi.encodeWithSignature(
                 "relayTokens(address,address,uint256,address)",
                 l1Token, // l1Token.
@@ -39,9 +45,5 @@ contract AdminRelayTokensAdapter is AdapterInterface {
 
     function relayTokens(address, address, uint256, address) external payable {
         revert("not supported");
-    }
-
-    function requiredAdminSalt() public pure returns (bytes32) {
-        return keccak256("SEND_VIA_ADMIN_RELAY_TOKENS_ADAPTER");
     }
 }
