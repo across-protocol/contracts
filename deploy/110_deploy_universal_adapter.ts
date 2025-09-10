39961bd70783d61e4e8b01d5a27f33d571d103e7
Ihor Farion <65650773+grasphoper@users.noreply.github.com>
2025-09-10 08:09:50 -0700

    improve: store across protocol deployments in `deployments.json` over `consts.ts` (#1101)
    * move AdapterStore from consts.ts to deployments.json
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * hubpoolstore
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * helios
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * fix throwOnError
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    ---------
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- deploy/110_deploy_universal_adapter.ts
+++ deploy/110_deploy_universal_adapter.ts
@@ -5,6 +5,7 @@ import { CIRCLE_DOMAIN_IDs, L1_ADDRESS_MAP, USDC, ZERO_ADDRESS } from "./consts"
 import { CCTP_NO_DOMAIN } from "@across-protocol/constants";
 import { CIRCLE_UNINITIALIZED_DOMAIN_ID } from "./consts";
 import assert from "assert";
+import { getDeployedAddress } from "../src/DeploymentUtils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { SPOKE_CHAIN_ID } = process.env;
@@ -15,23 +16,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   // Warning: re-using the same HubPoolStore for different L2's is only safe if the L2 spoke pools have
   // unique addresses, since the relayed message `targets` are part of the unique data hash.
-  const hubPoolStore = await hre.deployments.get("HubPoolStore");
+  const hubPoolStore = getDeployedAddress("HubPoolStore", chainId);
 
   // todo: implement similar treatment to `CIRCLE_DOMAIN_IDs`
   const oftDstEid = getOftEid(Number(SPOKE_CHAIN_ID));
   const oftFeeCap = toWei("1"); // 1 eth transfer fee cap
+  const adapterStore = getDeployedAddress("AdapterStore", chainId);
 
   const cctpDomainId = CIRCLE_DOMAIN_IDs[Number(SPOKE_CHAIN_ID)] ?? CCTP_NO_DOMAIN;
   const args = [
-    hubPoolStore.address,
+    hubPoolStore,
     USDC[chainId],
     cctpDomainId === CCTP_NO_DOMAIN ? ZERO_ADDRESS : L1_ADDRESS_MAP[chainId].cctpTokenMessenger,
     cctpDomainId === CCTP_NO_DOMAIN ? CIRCLE_UNINITIALIZED_DOMAIN_ID : cctpDomainId,
-    L1_ADDRESS_MAP[chainId].adapterStore,
+    adapterStore,
     oftDstEid,
     oftFeeCap,
   ];
-  const instance = await deployments.deploy("Universal_Adapter", {
+  const instance = await hre.deployments.deploy("Universal_Adapter", {
     from: deployer,
     log: true,
     skipIfAlreadyDeployed: false,
