1c11d8bb3938fdf449ede76450c02dd19eb41fcc
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-09-18 16:22:41 -0400

    Dropped optional param
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>

--- tasks/upgradeSpokePool.ts
+++ tasks/upgradeSpokePool.ts
@@ -3,7 +3,6 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 task("upgrade-spokepool", "Generate calldata to upgrade a SpokePool deployment")
   .addParam("implementation", "New SpokePool implementation address")
-  .addOptionalParam("upgradeOnly", "Upgrade only, do not pause/unpause deposits")
   .setAction(async function (args, hre: HardhatRuntimeEnvironment) {
     const { implementation, upgradeOnly } = args;
     if (!implementation) {
@@ -24,27 +23,23 @@ task("upgrade-spokepool", "Generate calldata to upgrade a SpokePool deployment")
     const spokePool = new ethers.Contract(implementation, abi);
 
     let calldata = "";
-    if (upgradeOnly) {
-      calldata = spokePool.interface.encodeFunctionData("upgradeTo", [implementation]);
-      console.log(`upgradeTo bytes: `, calldata);
-    } else {
-      /**
-       * We perform this seemingly unnecessary pause/unpause sequence because we want to ensure that the
-       * upgrade is successful and the new implementation gets forwarded calls by the proxy contract as expected
-       *
-       * Since the upgrade and call happens atomically, the upgrade will revert if the new implementation
-       * is not functioning correctly.
-       */
-      const data = spokePool.interface.encodeFunctionData("multicall", [
-        [
-          spokePool.interface.encodeFunctionData("pauseDeposits", [true]),
-          spokePool.interface.encodeFunctionData("pauseDeposits", [false]),
-        ],
-      ]);
 
-      calldata = spokePool.interface.encodeFunctionData("upgradeToAndCall", [implementation, data]);
-      console.log(`upgradeToAndCall bytes: `, calldata);
-    }
+    /**
+     * We perform this seemingly unnecessary pause/unpause sequence because we want to ensure that the
+     * upgrade is successful and the new implementation gets forwarded calls by the proxy contract as expected
+     *
+     * Since the upgrade and call happens atomically, the upgrade will revert if the new implementation
+     * is not functioning correctly.
+     */
+    const data = spokePool.interface.encodeFunctionData("multicall", [
+      [
+        spokePool.interface.encodeFunctionData("pauseDeposits", [true]),
+        spokePool.interface.encodeFunctionData("pauseDeposits", [false]),
+      ],
+    ]);
+
+    calldata = spokePool.interface.encodeFunctionData("upgradeToAndCall", [implementation, data]);
+    console.log(`upgradeToAndCall bytes: `, calldata);
 
     console.log(
       `Call relaySpokePoolAdminFunction() with the params [<chainId>, ${calldata}] on the hub pool from the owner's account.`
