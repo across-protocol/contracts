42a64468b7826665b1ca68a4e09edc42c8636cec
Dong-Ha Kim <dongha.kim210@gmail.com>
2025-09-30 02:45:27 +0200

    feat: hyperevm spoke pool periphery (#1124)
    * chore: HyperEVM SpokePoolPeriphery deployment
    
    * fixup

--- deploy/consts.ts
+++ deploy/consts.ts
@@ -187,6 +187,7 @@ export const L2_ADDRESS_MAP: { [key: number]: { [contractName: string]: string }
   [CHAIN_IDs.HYPEREVM]: {
     cctpV2TokenMessenger: "0x28b5a0e9C621a5BadaA536219b3a228C8168cf5d",
     cctpV2MessageTransmitter: "0x81D40F21F12A8F0E3252Bccb954D722d4c464B64",
+    permit2: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
   },
   [CHAIN_IDs.PLASMA]: {
     permit2: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
