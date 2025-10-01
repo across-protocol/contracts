abc1383ac1495e7acf20065c9c8942e2d8d3bcbf
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-10-01 10:36:02 -0400

    feat(Foundry): Updated constants usage (#1111)
    * feat(Foundry): Updated constants usage
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * Using chain family for zksync checks
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * Fixed type errors
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * use keccak to compare strings
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * using TESTNET_CHAIN_IDs instead
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * format
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * format 2
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * format constants
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * Added check for cctp
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * Moved constants to generated folder
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * using enum string representation for chain family
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    ---------
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>

--- script/027DeployScrollSpokePool.s.sol
+++ script/027DeployScrollSpokePool.s.sol
@@ -5,7 +5,6 @@ import { Script } from "forge-std/Script.sol";
 import { Test } from "forge-std/Test.sol";
 import { console } from "forge-std/console.sol";
 import { Scroll_SpokePool } from "../contracts/Scroll_SpokePool.sol";
-import { WETH9Interface } from "../contracts/external/interfaces/WETH9Interface.sol";
 import { DeploymentUtils } from "./utils/DeploymentUtils.sol";
 
 // How to run:
@@ -25,7 +24,7 @@ contract DeployScrollSpokePool is Script, Test, DeploymentUtils {
         console.log("HubPool address:", info.hubPool);
 
         // Get the appropriate addresses for this chain
-        WETH9Interface weth = getWrappedNativeToken(info.spokeChainId);
+        address weth = getWrappedNativeToken(info.spokeChainId);
 
         // Get L2 addresses for Scroll
         address l2GatewayRouter = getL2Address(info.spokeChainId, "scrollERC20GatewayRouter");
@@ -35,7 +34,7 @@ contract DeployScrollSpokePool is Script, Test, DeploymentUtils {
 
         // Prepare constructor arguments for Scroll_SpokePool
         bytes memory constructorArgs = abi.encode(
-            address(weth), // _wrappedNativeTokenAddress
+            weth, // _wrappedNativeTokenAddress
             QUOTE_TIME_BUFFER(), // _depositQuoteTimeBuffer
             FILL_DEADLINE_BUFFER() // _fillDeadlineBuffer
         );
@@ -64,7 +63,7 @@ contract DeployScrollSpokePool is Script, Test, DeploymentUtils {
         console.log("Chain ID:", info.spokeChainId);
         console.log("Hub Chain ID:", info.hubChainId);
         console.log("HubPool address:", info.hubPool);
-        console.log("WETH address:", address(weth));
+        console.log("WETH address:", weth);
         console.log("L2 Gateway Router:", l2GatewayRouter);
         console.log("L2 Scroll Messenger:", l2ScrollMessenger);
         console.log("Scroll_SpokePool proxy deployed to:", result.proxy);
--- script/utils/GenerateConstantsJson.ts
+++ script/utils/GenerateConstantsJson.ts
@@ -4,7 +4,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 // Import the constants from the TypeScript files
-import { CHAIN_IDs, PUBLIC_NETWORKS, TOKEN_SYMBOLS_MAP } from "../../utils/constants";
+import { CHAIN_IDs, PUBLIC_NETWORKS, TESTNET_CHAIN_IDs, TOKEN_SYMBOLS_MAP, ChainFamily } from "../../utils/constants";
 import {
   ZERO_ADDRESS,
   USDC,
@@ -23,6 +23,20 @@ import {
   L2_ADDRESS_MAP,
 } from "../../deploy/consts";
 
+const convertChainFamiliesEnumString = () => {
+  const publicNetworksWithEnum = Object.fromEntries(
+    Object.entries(PUBLIC_NETWORKS).map(([key, value]) => [
+      key,
+      {
+        ...value,
+        family: ChainFamily[value.family],
+      },
+    ])
+  );
+
+  return publicNetworksWithEnum;
+};
+
 /**
  * Convert the chain IDs object to the expected format
  * @returns { [key: string]: number }
@@ -34,15 +48,8 @@ import {
  *   "137": 137,
  * }
  */
-function convertChainIdsToObject(): { [key: string]: number } {
-  const result: { [key: string]: number } = {};
-  for (const [key, value] of Object.entries(CHAIN_IDs)) {
-    if (typeof value === "number") {
-      result[key] = value;
-    }
-  }
-  return result;
-}
+const convertChainIdsToObject = (): { [key: string]: number } =>
+  Object.fromEntries(Object.entries(CHAIN_IDs).map(([chainIdName, chainId]) => [chainIdName, chainId]));
 
 /**
  * Generate the wrapped native tokens for the public networks
@@ -79,8 +86,9 @@ function generateWrappedNativeTokens(): { [key: string]: string } {
 // Generate the constants.json structure
 function generateConstantsJson() {
   const constants = {
-    PUBLIC_NETWORKS,
+    PUBLIC_NETWORKS: convertChainFamiliesEnumString(),
     CHAIN_IDs: convertChainIdsToObject(),
+    TESTNET_CHAIN_IDs: Object.values(TESTNET_CHAIN_IDs),
     WETH,
     WRAPPED_NATIVE_TOKENS: generateWrappedNativeTokens(),
     L2_ADDRESS_MAP,
@@ -113,8 +121,8 @@ function main() {
 
     const constants = generateConstantsJson();
 
-    // Write to script/utils/constants.json
-    const outputPath = path.join(__dirname, "./constants.json");
+    // Write to generated/constants.json
+    const outputPath = "generated/constants.json";
     const outputDir = path.dirname(outputPath);
 
     // Ensure the directory exists
@@ -122,7 +130,7 @@ function main() {
       fs.mkdirSync(outputDir, { recursive: true });
     }
 
-    fs.writeFileSync(outputPath, JSON.stringify(constants, null, 2));
+    fs.writeFileSync(outputPath, JSON.stringify(constants, null, 2) + "\n");
 
     console.log(`✅ Successfully generated constants.json at ${outputPath}`);
     console.log(`📊 Generated ${Object.keys(constants.CHAIN_IDs).length} chain IDs`);
