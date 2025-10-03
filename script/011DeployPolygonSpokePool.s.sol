1700936da4fa21dadb1e06d7cbe35f954af20dfd
nicholaspai <npai.nyc@gmail.com>
2025-10-03 13:24:33 -0400

    deploy polygon spoke

--- script/011DeployPolygonSpokePool.s.sol
+++ script/011DeployPolygonSpokePool.s.sol
@@ -4,16 +4,17 @@ pragma solidity ^0.8.0;
 import { Script } from "forge-std/Script.sol";
 import { Test } from "forge-std/Test.sol";
 import { console } from "forge-std/console.sol";
-import { Arbitrum_SpokePool } from "../contracts/Arbitrum_SpokePool.sol";
+import { Polygon_SpokePool } from "../contracts/Polygon_SpokePool.sol";
+import { PolygonTokenBridger } from "../contracts/PolygonTokenBridger.sol";
 import { DeploymentUtils } from "./utils/DeploymentUtils.sol";
 
 // How to run:
 // 1. `source .env` where `.env` has MNEMONIC="x x x ... x"
-// 2. forge script script/008DeployArbitrumSpokePool.s.sol:DeployArbitrumSpokePool --rpc-url $NODE_URL_1 -vvvv
+// 2. forge script script/011DeployPolygonSpokePool.s.sol:DeployPolygonSpokePool --rpc-url $NODE_URL_137 -vvvv
 // 3. Verify the above works in simulation mode.
-// 4. Deploy with: forge script script/008DeployArbitrumSpokePool.s.sol:DeployArbitrumSpokePool --rpc-url $NODE_URL_1 --broadcast --verify
+// 4. Deploy with: forge script script/011DeployPolygonSpokePool.s.sol:DeployPolygonSpokePool --rpc-url $NODE_URL_137 --broadcast --verify
 
-contract DeployArbitrumSpokePool is Script, Test, DeploymentUtils {
+contract DeployPolygonSpokePool is Script, Test, DeploymentUtils {
     function run() external {
         string memory deployerMnemonic = vm.envString("MNEMONIC");
         uint256 deployerPrivateKey = vm.deriveKey(deployerMnemonic, 0);
@@ -22,17 +23,16 @@ contract DeployArbitrumSpokePool is Script, Test, DeploymentUtils {
         DeploymentInfo memory info = getSpokePoolDeploymentInfo(address(0));
 
         // Get the appropriate addresses for this chain
-        address weth = getWrappedNativeToken(info.spokeChainId);
+        address wmatic = getWrappedNativeToken(info.spokeChainId);
 
-        // Get L2 addresses for Arbitrum
-        address l2GatewayRouter = getL2Address(info.spokeChainId, "l2GatewayRouter");
+        // Get L2 addresses for Polygon
         address cctpTokenMessenger = getL2Address(info.spokeChainId, "cctpV2TokenMessenger");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        // Prepare constructor arguments for Arbitrum_SpokePool
+        // Prepare constructor arguments for Polygon_SpokePool
         bytes memory constructorArgs = abi.encode(
-            weth, // _weth
+            wmatic, // _wmatic
             QUOTE_TIME_BUFFER(), // _quoteTimeBuffer
             FILL_DEADLINE_BUFFER(), // _fillDeadlineBuffer
             getUSDCAddress(info.spokeChainId), // _usdc
@@ -43,17 +43,21 @@ contract DeployArbitrumSpokePool is Script, Test, DeploymentUtils {
 
         // Initialize deposit counter to very high number of deposits to avoid duplicate deposit ID's
         // with deprecated spoke pool.
+        address polygonTokenBridger = 0x0330E9b4D0325cCfF515E81DFbc7754F2a02ac57;
         bytes memory initArgs = abi.encodeWithSelector(
-            Arbitrum_SpokePool.initialize.selector,
+            Polygon_SpokePool.initialize.selector,
             1_000_000, // _initialDepositId
-            l2GatewayRouter, // _l2GatewayRouter
+            // The same token bridger must be deployed on mainnet and polygon, so its easier
+            // to reuse it.
+            PolygonTokenBridger(payable(polygonTokenBridger)), // _polygonTokenBridger
             info.hubPool, // _crossDomainAdmin
-            info.hubPool // _hubPool
+            info.hubPool, // _hubPool
+            getL2Address(info.spokeChainId, "fxChild") // _fxChild
         );
 
         // Deploy the proxy
         DeploymentResult memory result = deployNewProxy(
-            "Arbitrum_SpokePool",
+            "Polygon_SpokePool",
             constructorArgs,
             initArgs,
             true // implementationOnly
@@ -63,12 +67,13 @@ contract DeployArbitrumSpokePool is Script, Test, DeploymentUtils {
         console.log("Chain ID:", info.spokeChainId);
         console.log("Hub Chain ID:", info.hubChainId);
         console.log("HubPool address:", info.hubPool);
-        console.log("WETH address:", weth);
-        console.log("L2 Gateway Router:", l2GatewayRouter);
+        console.log("WMATIC address:", wmatic);
         console.log("CCTP Token Messenger:", cctpTokenMessenger);
         console.log("USDC address:", getUSDCAddress(info.spokeChainId));
-        console.log("Arbitrum_SpokePool proxy deployed to:", result.proxy);
-        console.log("Arbitrum_SpokePool implementation deployed to:", result.implementation);
+        console.log("PolygonTokenBridger address:", polygonTokenBridger);
+        console.log("FxChild address:", getL2Address(info.spokeChainId, "fxChild"));
+        console.log("Polygon_SpokePool proxy deployed to:", result.proxy);
+        console.log("Polygon_SpokePool implementation deployed to:", result.implementation);
 
         console.log("QUOTE_TIME_BUFFER()", QUOTE_TIME_BUFFER());
         console.log("FILL_DEADLINE_BUFFER()", FILL_DEADLINE_BUFFER());
