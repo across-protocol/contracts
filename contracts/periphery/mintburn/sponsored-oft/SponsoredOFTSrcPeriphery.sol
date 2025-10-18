828ad73195a2befe20166145d215df3c9c8b588d
Ihor Farion <ihor@umaproject.org>
2025-10-17 22:43:43 -0700

    updates
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- contracts/periphery/mintburn/sponsored-oft/SponsoredOFTSrcPeriphery.sol
+++ contracts/periphery/mintburn/sponsored-oft/SponsoredOFTSrcPeriphery.sol
@@ -8,25 +8,29 @@ import { ComposeMsgCodec } from "./ComposeMsgCodec.sol";
 import { IOFT, SendParam, MessagingFee } from "../../../interfaces/IOFT.sol";
 import { AddressToBytes32 } from "../../../libraries/AddressConverters.sol";
 import { MinimalLZOptions } from "../../../libraries/MinimalLZOptions.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // TODO? make Ownable and allow to change ApiPubKey and DstComposer. For Phase0, can keep it like this and just redeploy
 // This contract is to be used on source chain to route OFT sends through it. It's responsible for emitting an Across-
 // specific send events, checking the API signature and sending the transfer via OFT
-// TODO! Make this pausable
-contract SponsoredOFTSrcPeriphery {
+contract SponsoredOFTSrcPeriphery is Ownable {
     // TODO: instead of using `AddressToBytes32`, maybe just inline the function here? Feels more visible this way (no
     // one can accidentally break this contract by changing that function)
     using AddressToBytes32 for address;
     using MinimalLZOptions for bytes;
 
-    // @dev gas savings.
     bytes public constant EMPTY_MSG_BYTES = new bytes(0);
 
-    // TODO? which of these should be non-immutable?
-    address public immutable TOKEN; // token that is sendable over `oftMessenger`
+    address public immutable TOKEN;
     address public immutable OFT_MESSENGER;
-    address public immutable API_PUBKEY; // pubKey that's used for ECDSA signing from the API side
-    address public immutable DST_COMPOSER;
+    // Destination endpoint id. Immutable because we only support one `dstHandler`
+    uint32 public immutable DST_EID;
+
+    // Signer public key to check the sig against. Signed off by Across API
+    address public signer;
+
+    // Destination handler address
+    address public dstHandler;
 
     mapping(bytes32 => bool) public quoteNonces;
 
@@ -42,19 +46,19 @@ contract SponsoredOFTSrcPeriphery {
         bytes sig
     );
 
-    // TODO: make Ownable and allow to change ApiPubKey and DstComposer
-    constructor(address token, address oftMessenger, address apiPubKey, address dstComposer) {
-        TOKEN = token;
-        OFT_MESSENGER = oftMessenger;
-        API_PUBKEY = apiPubKey;
-        DST_COMPOSER = dstComposer;
+    constructor(address _token, address _oftMessenger, address _signer, address _dstHandler, uint32 _dstEid) {
+        TOKEN = _token;
+        OFT_MESSENGER = _oftMessenger;
+        signer = _signer;
+        dstHandler = _dstHandler;
+        DST_EID = _dstEid;
     }
 
     // @dev The API recommended the user set some `msg.value` in order to pay OFT fee for this transfer. If it's not
     // enough to cover `fee.nativeFee`, we revert. If `msg.value > nativeFee`, `refundAddress` receives excess
     function deposit(Quote calldata quote, bytes calldata signature) external payable {
         // Step 1: check that the quote is signed correctly
-        require(QuoteSignLib.isSignatureValid(API_PUBKEY, quote.signedParams, signature), "Incorrect signature");
+        require(QuoteSignLib.isSignatureValid(signer, quote.signedParams, signature), "Incorrect signature");
 
         // Step 2: check that the quote params make sense: e.g. quoteDeadline is not hit, quote nonce is unique
         require(quote.signedParams.deadline <= block.timestamp, "quote expired");
@@ -63,6 +67,8 @@ contract SponsoredOFTSrcPeriphery {
 
         // Step 3: build oft send params from quote
         (SendParam memory sendParam, MessagingFee memory fee, address refundAddress) = _buildOftTransfer(quote);
+
+        // TODO: pull tokens from sender and approve the OFT_MESSENGER
 
         // Step 4: send oft transfer
         IOFT(OFT_MESSENGER).send(sendParam, fee, refundAddress);
@@ -82,8 +88,7 @@ contract SponsoredOFTSrcPeriphery {
     function _buildOftTransfer(
         Quote calldata quote
     ) internal view returns (SendParam memory, MessagingFee memory, address) {
-        // TODO? consider passing in composer as a part of quote
-        bytes32 to = DST_COMPOSER.toBytes32();
+        bytes32 to = dstHandler.toBytes32();
 
         bytes memory composeMsg = ComposeMsgCodec._encode(
             quote.signedParams.nonce,
@@ -100,6 +105,10 @@ contract SponsoredOFTSrcPeriphery {
             .newOptions()
             .addExecutorLzReceiveOption(uint128(50_000), uint128(0))
             .addExecutorLzComposeOption(uint16(0), uint128(200_000), uint128(0));
+
+        if (quote.signedParams.dstEid != DST_EID) {
+            revert("Incorrect dstEid");
+        }
 
         SendParam memory sendParam = SendParam(
             quote.signedParams.dstEid,
@@ -118,5 +127,13 @@ contract SponsoredOFTSrcPeriphery {
         MessagingFee memory fee = IOFT(OFT_MESSENGER).quoteSend(sendParam, false);
 
         return (sendParam, fee, quote.unsignedParams.refundRecipient);
+    }
+
+    function setSigner(address _newSigner) external onlyOwner {
+        signer = _newSigner;
+    }
+
+    function setDstHandler(address _newDstHandler) external onlyOwner {
+        dstHandler = _newDstHandler;
     }
 }
