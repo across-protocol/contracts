25f9f0239104f605f9d997986a1c9b844d7a206f
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-10-27 19:04:09 -0400

    feat: Use deplyed-addresses as source of truth
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>

--- script/utils/ExtractDeployedFoundryAddresses.ts
+++ script/utils/ExtractDeployedFoundryAddresses.ts
@@ -45,8 +45,8 @@ interface JsonOutput {
       contracts: {
         [contractName: string]: {
           address: string;
-          transaction_hash: string;
-          block_number: number | null;
+          transaction_hash?: string;
+          block_number?: number;
         };
       };
     };
@@ -341,7 +341,9 @@ function generateAddressesFile(broadcastFiles: BroadcastFile[], outputFile: stri
 
       for (const contract of contracts) {
         content.push(`- **${contract.contractName}**: \`${contract.contractAddress}\``);
-        content.push(`  - Transaction Hash: \`${contract.transactionHash}\``);
+        if (contract.transactionHash !== "Unknown") {
+          content.push(`  - Transaction Hash: \`${contract.transactionHash}\``);
+        }
         if (contract.blockNumber !== null) {
           content.push(`  - Block Number: \`${contract.blockNumber}\``);
         }
@@ -366,8 +368,8 @@ function generateAddressesFile(broadcastFiles: BroadcastFile[], outputFile: stri
         const contractName = contract.contractName;
         jsonOutput.chains[chainId].contracts[contractName] = {
           address: contract.contractAddress,
-          transaction_hash: contract.transactionHash,
-          block_number: contract.blockNumber,
+          ...(contract.blockNumber !== null && { block_number: contract.blockNumber }),
+          ...(contract.transactionHash !== "Unknown" && { transaction_hash: contract.transactionHash }),
         };
       }
     }
