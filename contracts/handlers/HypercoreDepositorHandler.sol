a7790bd614ffea8a14d6d9ef2ed6472fab17ac50
nicholaspai <npai.nyc@gmail.com>
2025-11-18 14:31:43 -0500

    add safeTransferFrom
    Signed-off-by: nicholaspai <npai.nyc@gmail.com>

--- contracts/handlers/HypercoreDepositorHandler.sol
+++ contracts/handlers/HypercoreDepositorHandler.sol
@@ -9,23 +9,25 @@ import { CoreWriterLib, PrecompileLib } from "hyper-evm-lib/src/CoreWriterLib.so
 import { HLConversions } from "hyper-evm-lib/src/common/HLConversions.sol";
 
 /**
- * @title Allows caller to bridge tokens from HyperEVM to Hypercore and send them to the end user's account
+ * @title Allows caller to bridge tokens from HyperEVM to Hypercore and send them to an end user's account
  * on Hypercore.
  * @dev This contract should only be deployed on HyperEVM.
  * @dev This contract can replace a MulticallHandler on HyperEVM if the intent only wants to deposit tokens into
  * Hypercore and bypass the other complex arbitrary calldata logic.
- * @dev This contract can also be called by the MulticallHandler to deposit tokens into Hypercore.
+ * @dev This contract can also be called directly to deposit tokens into Hypercore on behalf of an end user.
  */
 contract HypercoreDepositorHandler is AcrossMessageHandler, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     /**
      * @notice Bridges tokens from HyperEVM to Hypercore and sends them to the end user's account on Hypercore.
+     * @dev Requires msg.sender to have approved this contract to spend the tokens.
      * @param token The address of the token to deposit.
      * @param amount The amount of tokens on HyperEVM to deposit.
      * @param user The address of the user on Hypercore to send the tokens to.
      */
     function depositToHypercore(address token, uint256 amount, address user) external nonReentrant {
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
         _bridgeToCore(token, amount);
         _spotSend(user, token, amount);
     }
