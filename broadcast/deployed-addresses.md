24c3cf84222450c955d67928029936beefd4a022
Taylor Webb <tbwebb22@gmail.com>
2025-11-25 09:23:17 -0500

    deploy SpokePool to BSC
    Signed-off-by: Taylor Webb <tbwebb22@gmail.com>

--- broadcast/deployed-addresses.md
+++ broadcast/deployed-addresses.md
@@ -314,8 +314,8 @@ This file contains the latest deployed smart contract addresses from the broadca
 
 #### Helios
 
-- **Helios**: `0xE58480CA74f1A819faFd777BEDED4E2D5629943d`
-  - Block Number: `59344945`
+- **Helios**: `0x19256DCEa4B63c56B3EFc8708cd62F595B2d1922`
+  - Block Number: `69435667`
 
 ### Unichain (Chain ID: 130)
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -145,11 +145,16 @@ const config: HardhatUserConfig = {
     "arbitrum-sepolia": getDefaultHardhatConfig(CHAIN_IDs.ARBITRUM_SEPOLIA, true),
     sepolia: getDefaultHardhatConfig(CHAIN_IDs.SEPOLIA, true),
     polygon: getDefaultHardhatConfig(CHAIN_IDs.POLYGON),
-    bsc: getDefaultHardhatConfig(CHAIN_IDs.BSC),
-    // ! Notice. Params below helped deploy Universal_Spoke on BSC, but might not be desirable always
-    // gas: "auto",
-    // gasPrice: 3e8, // 0.3 GWEI
-    // gasMultiplier: 4.0,
+    bsc: {
+      chainId: CHAIN_IDs.BSC,
+      url: getNodeUrl(CHAIN_IDs.BSC),
+      accounts: { mnemonic },
+      saveDeployments: true,
+      companionNetworks: { l1: "mainnet" },
+      gas: "auto",
+      gasPrice: 3e8, // 0.3 GWEI
+      gasMultiplier: 4.0,
+    },
     hyperevm: getDefaultHardhatConfig(CHAIN_IDs.HYPEREVM),
     monad: getDefaultHardhatConfig(CHAIN_IDs.MONAD),
     "polygon-amoy": getDefaultHardhatConfig(CHAIN_IDs.POLYGON_AMOY),
