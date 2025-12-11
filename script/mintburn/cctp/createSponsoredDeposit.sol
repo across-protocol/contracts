c673e42b6a1a29d3ea6ae96a452ff9b401823cce
Ihor Farion <65650773+grasphoper@users.noreply.github.com>
2025-12-11 14:38:13 -0800

    feat: *NEW* preprod deployments (#1212)
    * add OFT track deployment + README updates
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * CCTP deployment
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * remaining CCTP src deployments
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * CCTP Dst peripehry deploy
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * README + version
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * add src peripheries
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * Monad and polygon deploy
    
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    
    * bump package
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * fix: generate typechain but skip tests (#1213)
    
    * try not blanket skip in CI
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * upd
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * bump version
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    ---------
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    ---------
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    Signed-off-by: Faisal Usmani <faisal.of.usmani@gmail.com>
    Co-authored-by: Faisal Usmani <faisal.of.usmani@gmail.com>

--- script/mintburn/cctp/createSponsoredDeposit.sol
+++ script/mintburn/cctp/createSponsoredDeposit.sol
@@ -85,16 +85,16 @@ contract CreateSponsoredDeposit is DeploymentUtils {
         SponsoredCCTPInterface.SponsoredCCTPQuote memory quote = SponsoredCCTPInterface.SponsoredCCTPQuote({
             sourceDomain: config.get("cctpDomainId").toUint32(), // Arbitrum CCTP domain
             destinationDomain: 19, // HyperEVM CCTP domain
-            mintRecipient: address(0xb63c02e60C05F05975653edC83F876C334E07C6d).toBytes32(), // Destination handler contract
-            amount: 10000, // 100 USDC (6 decimals)
+            mintRecipient: address(0x1c709Fd0Db6A6B877Ddb19ae3D485B7b4ADD879f).toBytes32(), // Destination handler contract
+            amount: 1000000, // 100 USDC (6 decimals)
             burnToken: config.get("usdc").toAddress().toBytes32(), // USDC on Arbitrum
-            destinationCaller: address(0xb63c02e60C05F05975653edC83F876C334E07C6d).toBytes32(), // Destination handler contract
-            maxFee: 1, // 0 max fee
+            destinationCaller: address(0x1c709Fd0Db6A6B877Ddb19ae3D485B7b4ADD879f).toBytes32(), // Destination handler contract
+            maxFee: 100, // 0 max fee
             minFinalityThreshold: 1000, // Minimum finality threshold
             nonce: keccak256(abi.encodePacked(block.timestamp, deployer, vm.getNonce(deployer))), // Generate nonce
             deadline: block.timestamp + 10800, // 3 hours from now
             maxBpsToSponsor: 400, // 4% max sponsorship (400 basis points)
-            maxUserSlippageBps: 0, // 4% max user slippage (400 basis points)
+            maxUserSlippageBps: 400, // 4% max user slippage (400 basis points)
             finalRecipient: address(0x9A8f92a830A5cB89a3816e3D267CB7791c16b04D).toBytes32(), // Final recipient
             finalToken: address(0xb88339CB7199b77E23DB6E890353E22632Ba630f).toBytes32(), // USDC on HyperEVM
             executionMode: uint8(SponsoredCCTPInterface.ExecutionMode.DirectToCore), // DirectToCore mode
@@ -151,8 +151,10 @@ contract CreateSponsoredDeposit is DeploymentUtils {
         // Call depositForBurn
         vm.startBroadcast(deployerPrivateKey);
 
+        // IERC20(config.get("usdc").toAddress()).approve(address(sponsoredCCTPSrcPeriphery), 11000000);
+
         console.log("Calling depositForBurn...");
-        sponsoredCCTPSrcPeriphery.depositForBurn(quote, signature);
+        sponsoredCCTPSrcPeriphery.depositForBurn{ gas: 1000000 }(quote, signature);
 
         console.log("Transaction completed successfully!");
 
