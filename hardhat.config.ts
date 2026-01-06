2f5bbfa81b21051ed069bdfecb71fc9b7fd1e485
Taylor Webb <tbwebb22@gmail.com>
2026-01-06 14:08:24 -0700

    exclude SP1Helios from hardhat compilation
    Signed-off-by: Taylor Webb <tbwebb22@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,10 +4,13 @@ const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require("hardhat/builtin-task
 subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(async (_: any, __: any, runSuper: any) => {
   const paths = await runSuper();
 
+  // Filter out sp1-helios contracts (uses Foundry-only git submodule imports)
+  const filteredPaths = paths.filter((p: any) => !p.includes("contracts/sp1-helios"));
+
   // Filter out files that cause problems when using "paris" hardfork (currently used to compile everything when IS_TEST=true)
   // Reference: https://github.com/NomicFoundation/hardhat/issues/2306#issuecomment-1039452928
   if (process.env.IS_TEST === "true") {
-    return paths.filter((p: any) => {
+    return filteredPaths.filter((p: any) => {
       return (
         !p.includes("contracts/periphery/mintburn") &&
         !p.includes("contracts/external/libraries/BytesLib.sol") &&
@@ -17,7 +20,7 @@ subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(async (_: any, __: any
     });
   }
 
-  return paths;
+  return filteredPaths;
 });
 
 import * as dotenv from "dotenv";
