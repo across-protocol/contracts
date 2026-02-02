63055a8f05b9035424068af0a3a8a989e7a1757c
Ihor Farion <ihor@umaproject.org>
2026-02-02 15:36:10 -0800

    ploish
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- contracts/usdfree/Interfaces4.sol
+++ contracts/usdfree/Interfaces4.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.30;
 
 struct ExecutionStep {
     bytes tokenReq; // MANDATORY: (token, amount). Token has to be correct. Amount can be 0 to mean "no enforcement"
+    // NOTE: these are not mandatory. They can go into the otherStaticReqs, or can be included here for simplicity
     bytes submitterReq; // None OR address (can be bytes32 for non-EVM chains)
     bytes deadlineReq; // None OR deadline
     // A list of other requiremens that can be checked in a static way after the submitter has already executed
     bytes[] otherStaticReqs;
-    // NOTE: if we want obfuscation to encompass both the requirements and the actions, we can record
-    // auction `Changes` and apply them after the deobfuscation
-    // User-defined action that will be executed with the balance of tokenReq.token
-    bytes hashOrFinalAction; // can be obfuscated
+    // NOTE: if we want obfuscation to encompass both the requirements and the final action, we can record auction `Changes` 
+    // and apply them after the deobfuscation
+    bytes hashOrFinalAction;
     // Recipient that can withdraw funds after the deadline if the order hasn't been submitted yet
     bytes32 refundRecipient;
 }
@@ -28,16 +28,20 @@ struct OrderWithAuction {
     address auctionAuthority;
 }
 
+// A struct that represents some change in user requirement as a result of the auction
 struct Change {
-    // index of the step this change belongs to
-    uint8 stepIndex;
-    // token, deadline or submitter change
-    bytes change;
+    // enum for: token, deadline, submitter or custom
+    uint8 typ;
+    // NOTE: if a type is custom, data can inlcude an index of the otherStaticReqs that the auction wants to change. The
+    // funtion to apply the change has to be implemented on the OrderGateway for this to work
+    // data gets interpreted by the Gateway to apply the change depending on the type
+    bytes data;
 }
 
 // Auction changes can do things like change the submitter, improve the token requirement or shorten the deadline etc.
 struct AuctionChanges {
-    Change[] changes;
+    // changes[idx] are relevant to the requirements of order.steps[idx]
+    Change[][] changes;
     // sig is over (orderId, changes) by the auction authority specified in OrderWithAuction
     bytes sig;
 }
@@ -47,7 +51,7 @@ struct TokenAmount {
     uint256 amount;
 }
 
-// Provided to support the execution of a singel step
+// Provided to support the execution of a single step
 struct SubmitterData {
     TokenAmount[] extraFunding;
     bytes executions; // weiroll
@@ -65,20 +69,19 @@ contract OrderGateway {
 }
 
 contract Executor {
-    // TODO: IMO it makes sense to have executor check submitter requirement too. I don't think it matters much though
     function execute(
-        // NOTE: submitter is propagated by the caller: either Gateway or `IntentStore`
+        // `submitter` is propagated by the caller: either Gateway or `IntentStore`
         address submitter;
-        // NOTE: these are provided by the submitter to try to meet user's requirements
+        // Provided by the submitter to try to meet user's requirements
         bytes submitterExecutions; // weiroll
-        // NOTE: these are taken from the current `ExecutionStep`
+        // Taken from the current `ExecutionStep`
         bytes tokenReq,
         bytes submitterReq,
         bytes deadlineReq,
         // A list of other requiremens that can be checked in a static way after the submitter has already executed
         bytes[] otherStaticReqs;
         bytes memory finalActionParams,
-        // NOTE: these are the remaining execution steps, initially defined by the user in `Order.steps`
+        // The remaining execution steps, initially defined by the user in `Order.steps`
         ExecutionStep[] nextSteps
     ) external;
 }
@@ -86,13 +89,13 @@ contract Executor {
 // NOTE: it is a task of `FinalUserActionExecutor` to propagate `nextSteps` to the future execution layer
 interface FinalUserActionExecutor {
     function executeFinal(
-        // NOTE: this is taken from `ExecutionStep.tokenReq.token`
+        // this is taken from `ExecutionStep.tokenReq.token`
         address token,
-        // NOTE: this is taken from `balanceOf(executorContract, ExecutionStep.tokenReq.token)`
+        // this is taken from `balanceOf(executorContract, ExecutionStep.tokenReq.token)`
         uint256 amount,
-        // NOTE: these are hardcoded in the `ExecutionStep.hashOrFinalAction`
+        // these are hardcoded in the `ExecutionStep.hashOrFinalAction`
         bytes memory finalActionParams,
-        // NOTE: these are the remaining execution steps, initially defined by the user in `Order.steps`
+        // these are the remaining execution steps, initially defined by the user in `Order.steps`
         ExecutionStep[] nextSteps
-    ) external;
+    ) external payable;
 }
