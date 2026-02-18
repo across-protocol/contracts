184ac3372421e942d7ebabdc2a661843d56ff9b4
Taylor Webb <tbwebb22@gmail.com>
2026-02-18 16:03:40 -0700

    add counterfactual contracts to hardhat test compilation exclusion

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,6 +15,7 @@ subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(async (_: any, __: any
   if (isTest) {
     return filteredPaths.filter((p: any) => {
       return (
+        !p.includes("contracts/periphery/counterfactual") &&
         !p.includes("contracts/periphery/mintburn") &&
         !p.includes("contracts/external/libraries/BytesLib.sol") &&
         !p.includes("contracts/libraries/SponsoredCCTPQuoteLib.sol") &&
