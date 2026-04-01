91d3c27196dbea1bbb9a5460b0cea043c848d0f2
Matt Rice <matthewcrice32@gmail.com>
2026-04-01 19:28:01 -0400

    Replace Transfer event with Proxy event in TransferProxy
    This will help link depositIds to CCTP and other non Across txfers.

--- contracts/TransferProxy.sol
+++ contracts/TransferProxy.sol
@@ -27,10 +27,10 @@ contract TransferProxy {
     error InvalidOutputToken();
     error InvalidOutputAmount();
 
-    event Transfer(address indexed inputToken, address indexed recipient, uint256 inputAmount);
+    event Proxy(address indexed depositor, uint256 indexed depositNonce, address indexed inputToken, uint256 inputAmount);
 
     function deposit(
-        bytes32, // depositor
+        bytes32 depositor
         bytes32 recipient,
         bytes32 inputToken,
         bytes32 outputToken,
@@ -43,11 +43,12 @@ contract TransferProxy {
         uint32, // exclusivityDeadline
         bytes calldata message
     ) external {
+        emit Proxy(depositor, 0, inputToken, inputAmount);
         _transfer(inputToken, outputToken, recipient, inputAmount, outputAmount, destinationChainId, message);
     }
 
     function unsafeDeposit(
-        bytes32, // depositor
+        bytes32, depositor
         bytes32 recipient,
         bytes32 inputToken,
         bytes32 outputToken,
@@ -61,6 +62,7 @@ contract TransferProxy {
         uint32, // exclusivityParameter
         bytes calldata message
     ) external {
+        emit Proxy(depositor, depositNonce, inputToken, inputAmount);
         _transfer(inputToken, outputToken, recipient, inputAmount, outputAmount, destinationChainId, message);
     }
 
@@ -79,7 +81,6 @@ contract TransferProxy {
         address token = inputToken.toAddress();
         address to = recipient.toAddress();
         IERC20(token).safeTransferFrom(msg.sender, to, inputAmount);
-        emit Transfer(token, to, inputAmount);
 
         if (message.length > 0 && to.isContract()) {
             AcrossMessageHandler(to).handleV3AcrossMessage(token, inputAmount, msg.sender, message);
