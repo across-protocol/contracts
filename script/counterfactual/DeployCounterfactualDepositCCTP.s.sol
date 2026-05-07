86ed7cbb96a087244467c5ecf4e30f050b9b1056
Taylor Webb <tbwebb22@gmail.com>
2026-05-07 11:05:43 -0600

    revert deploy script changes

--- script/counterfactual/DeployCounterfactualDepositCCTP.s.sol
+++ script/counterfactual/DeployCounterfactualDepositCCTP.s.sol
@@ -16,14 +16,10 @@ contract DeployCounterfactualDepositCCTP is CounterfactualConfig {
         require(hasCctpDomain(block.chainid), "Chain does not support CCTP");
         address srcPeriphery = _resolveCctpPeriphery();
         require(srcPeriphery != address(0), "CCTP periphery not deployed on this chain");
-        _run(srcPeriphery, getCircleDomainId(block.chainid));
+        this.run(srcPeriphery, getCircleDomainId(block.chainid));
     }
 
     function run(address srcPeriphery, uint32 sourceDomain) external {
-        _run(srcPeriphery, sourceDomain);
-    }
-
-    function _run(address srcPeriphery, uint32 sourceDomain) internal {
         string memory deployerMnemonic = vm.envString("MNEMONIC");
         uint256 deployerPrivateKey = vm.deriveKey(deployerMnemonic, 0);
 
